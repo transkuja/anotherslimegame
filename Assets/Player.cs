a59c189e6f5ef5dde01bd3dde3de88421c926443
Unknown <transkuja@hotmail.fr>
2017-10-06 01:11:27 +0200

    Start implementing jump, always jump before move

---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody rb;
+
+    public Rigidbody Rb
+    {
+        get
+        {
+            return rb;
+        }
+
+        set
+        {
+            rb = value;
+        }
+    }
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using XInputDotNetPure;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+public class PlayerController : MonoBehaviour {
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+    [SerializeField]
+    int maxJumpFrames = 30;
+    int currentJumpHeight = 0;
+    Player player;
+    [SerializeField]
+    float jumpPower = 7.0f;
+
+    private void Start()
+    {
+        player = GetComponent<Player>();
+        if (player == null)
+            Debug.LogWarning("Player component should not be null");
+    }
+
+    void Update () {
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+
+        if (state.Buttons.A == ButtonState.Pressed && currentJumpHeight < maxJumpFrames)
+            Jump();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Ground>() != null)
+            currentJumpHeight = 0;
+    }
+
+    void Jump()
+    {
+        player.Rb.AddForce(Vector3.up * jumpPower);
+        currentJumpHeight++;
+    }
+}
