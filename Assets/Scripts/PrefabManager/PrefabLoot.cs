dd308041fb274f994676f85d2ae990a68e292204
Unknown <remi.boscher@gmail.com>
2017-10-09 17:17:11 +0200

    PrefabsUtils

---
+++ Assets/Scripts/PrefabManager/PrefabLoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabLoot : MonoBehaviour {
+
+    private static PrefabLoot instance;
+
+    public PrefabLoot Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(instance);
+            }
+            return instance;
+        }
+
+        private set { }
+    }
+
+    [SerializeField]
+    public GameObject prefabCollectable1;
+
+    public GameObject spawnCollectable1Instance(Vector3 where, Quaternion direction, Transform parent)
+    {
+        return Instantiate(Instance.prefabCollectable1, where, direction, parent);
+    }
+
+
+    [SerializeField]
+    public GameObject prefabCollectable2;
+
+    public GameObject spawnCollectable2Instance(Vector3 where, Quaternion direction, Transform parent)
+    {
+        return Instantiate(Instance.prefabCollectable2, where, direction, parent);
+    }
+}
