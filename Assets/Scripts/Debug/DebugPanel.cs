11305c4318254c7fdfe1507b5bba4bbe1619ff35
Unknown <transkuja@hotmail.fr>
2017-10-11 14:03:24 +0200

    Create Debug Tools script, but had to move because of Gwen

--- Assets/Scripts/Debug/DebugPanel.cs
+++ Assets/Scripts/Debug/DebugPanel.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class DebugPanel : MonoBehaviour {
+    enum DebugPanelChildren { EvolutionsState, CollectablesState, PlayerInfo };
 
-	// Use this for initialization
-	void Start () {
+    Transform evolutions;
+    Transform collectables;
+    Transform playerInfo;
+    Text evolutionsText;
+    Text collectablesText;
+    Text playerInfoText;
+
+    void Start () {
+        evolutions = transform.GetChild((int)DebugPanelChildren.EvolutionsState);
+        collectables = transform.GetChild((int)DebugPanelChildren.CollectablesState);
+        playerInfo = transform.GetChild((int)DebugPanelChildren.PlayerInfo);
+        evolutionsText = evolutions.GetComponent<Text>();
+        collectablesText = collectables.GetComponent<Text>();
+        playerInfoText = playerInfo.GetComponent<Text>();
+    }
+
+    void Update () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
---
+++ Assets/Scripts/Debug/DebugTools.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTools : MonoBehaviour {
+
+    public static bool isDebugModeActive = false;
+
+    [SerializeField]
+    Transform debugPanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
