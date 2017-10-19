465c7d4f2d4c8f5d95d14ed361b1500925aa5020
Unknown <remi.boscher@gmail.com>
2017-10-20 01:29:59 +0200

    Un tapis c'est jolie

---
+++ Assets/Scripts/TMP_Script/UnlockDoor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockDoor : MonoBehaviour
+{
+    public Text timer;
+    public int minute;
+    public int secondes;
+
+    // Use this for initialization
+    void Start () {
+        minute = 1;
+        secondes = 0;
+        timer.text = "0" + minute + " : 0" + secondes;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
