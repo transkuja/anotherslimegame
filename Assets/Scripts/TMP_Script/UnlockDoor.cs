9191158e612e58b1d6f7f626d6112f629ad4c486
Unknown <remi.boscher@gmail.com>
2017-10-20 02:31:24 +0200

    Door behavior and test graph's arena

--- Assets/Scripts/TMP_Script/UnlockDoor.cs
+++ Assets/Scripts/TMP_Script/UnlockDoor.cs
@@ -6,18 +6,38 @@ using UnityEngine.UI;
 public class UnlockDoor : MonoBehaviour
 {
     public Text timer;
-    public int minute;
-    public int secondes;
+    private int minutes;
+    private int seconds;
+    public float time= 50.0f;
+
+    public GameObject porte;
 
     // Use this for initialization
     void Start () {
-        minute = 1;
-        secondes = 0;
-        timer.text = "0" + minute + " : 0" + secondes;
+
+        minutes = Mathf.FloorToInt(time / 60); 
+        seconds = (int)time % 60; 
+
+        timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (time > 0.0f)
+        {
+            time -= Time.deltaTime;
+
+            minutes = Mathf.FloorToInt(time / 60);
+            seconds = (int)time % 60;
+
+            timer.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+        }
+        else
+        {
+            porte.SetActive(false);
+        }
+   
+
+
+    }
 }
