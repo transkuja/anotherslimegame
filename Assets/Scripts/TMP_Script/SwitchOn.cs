29f276bc6120bb8d3513eefc6ee7b6ade4bfe375
Unknown <remi.boscher@gmail.com>
2017-10-20 09:35:19 +0200

    SwitchOnDoor

---
+++ Assets/Scripts/TMP_Script/SwitchOn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchOn : MonoBehaviour {
+    public bool isSwitchOn = false;
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.GetComponent<Player>())
+        {
+            isSwitchOn = true;
+        }
+    }
+}
--- Assets/Scripts/TMP_Script/UnlockDoor.cs
+++ Assets/Scripts/TMP_Script/UnlockDoor.cs
@@ -5,38 +5,55 @@ using UnityEngine.UI;
 
 public class UnlockDoor : MonoBehaviour
 {
+    public bool isTimedDoor = false;
+
+    [Header("Timed Door")]
     public Text timer;
     private int minutes;
     private int seconds;
     public float time= 50.0f;
 
+    [Header("Switched Door")]
+    public GameObject refSwitch;
+
     public GameObject porte;
 
     // Use this for initialization
     void Start () {
+        if (isTimedDoor)
+        {
+            minutes = Mathf.FloorToInt(time / 60);
+            seconds = (int)time % 60;
 
-        minutes = Mathf.FloorToInt(time / 60); 
-        seconds = (int)time % 60; 
+            timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
 
-        timer.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (time > 0.0f)
+        if (isTimedDoor)
         {
-            time -= Time.deltaTime;
+            if (time > 0.0f)
+            {
+                time -= Time.deltaTime;
 
-            minutes = Mathf.FloorToInt(time / 60);
-            seconds = (int)time % 60;
+                minutes = Mathf.FloorToInt(time / 60);
+                seconds = (int)time % 60;
 
-            timer.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+                timer.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+            }
+            else
+            {
+                porte.SetActive(false);
+            }
         }
         else
         {
-            porte.SetActive(false);
+            if(refSwitch.GetComponent<SwitchOn>().isSwitchOn)
+                porte.SetActive(false);
         }
-   
+
 
 
     }
