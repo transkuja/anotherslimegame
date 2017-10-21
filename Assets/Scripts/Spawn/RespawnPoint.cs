7be78cc5ca015101833ef15d64df42832a65b257
Unknown <transkuja@hotmail.fr>
2017-10-22 00:06:54 +0200

    Add respawn point prefab (LD should be updated with them)

---
+++ Assets/Scripts/Spawn/RespawnPoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+/*
+ * Respawn points should be placed properly on entries/exits where we want the player to respawn
+ */
+public class RespawnPoint : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>() != null)
+            other.GetComponent<Player>().respawnPoint = transform;
+    }
+}
