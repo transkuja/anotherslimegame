21395c4260133768b3b273dd86305096d23c8f0a
Unknown <transkuja@hotmail.fr>
2017-10-21 22:51:22 +0200

    Handle respawn when falling

---
+++ Assets/Scripts/Spawn/DeathZone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class DeathZone : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>() != null)
+        {
+            RespawnProcess(other.GetComponent<Player>());
+        }
+    }
+
+    /*
+     * Contains specific respawn rules
+     */
+    void RespawnProcess(Player player)
+    {
+        player.transform.position = player.respawnPoint.position;
+        player.transform.rotation = player.respawnPoint.rotation;
+    }
+
+}
