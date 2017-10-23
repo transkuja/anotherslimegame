65fc2537884e62d75d4abf8f8e8dd0074245bd8e
Unknown <Sebdami@hotmail.fr>
2017-10-23 15:13:02 +0200

    Add blob shadow on players

---
+++ Assets/Scripts/ShadowClipper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowClipper : MonoBehaviour {
+
+    Projector shadowProjector;
+    float shadowDistanceTolerance  = 0.5f;
+     
+    private float origNearClipPlane;
+    private float origFarClipPlane;
+     
+    void Start()
+    {
+        if (!shadowProjector) shadowProjector = transform.GetComponent<Projector>();
+        origNearClipPlane = shadowProjector.nearClipPlane;
+        origFarClipPlane = shadowProjector.farClipPlane;
+    }
+
+    void Update()
+    {
+        Ray ray = new Ray(shadowProjector.transform.position
+                + shadowProjector.transform.forward.normalized * origNearClipPlane,
+                shadowProjector.transform.forward);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, origFarClipPlane - origNearClipPlane,
+                ~shadowProjector.ignoreLayers))
+        {
+            float dist = hit.distance + origNearClipPlane;
+            shadowProjector.nearClipPlane = Mathf.Max(dist - shadowDistanceTolerance, 0);
+            shadowProjector.farClipPlane = dist + shadowDistanceTolerance;
+        }
+    }
+}
