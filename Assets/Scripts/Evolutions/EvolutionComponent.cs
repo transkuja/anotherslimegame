3d94b96904e67708c92c9eff6dde3b3852bcab2d
Unknown <olivier.serris@wanadoo.fr>
2017-10-23 18:56:58 +0200

    StrenghEvolution firstDraw

--- Assets/Scripts/Evolutions/EvolutionComponent.cs
+++ Assets/Scripts/Evolutions/EvolutionComponent.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
+
 
 public class EvolutionComponent : MonoBehaviour {
     Evolution evolution;
     float timer;
     bool isEvolutionInitialized = false;
     bool isTimerInitialized = false;
+    protected PlayerController playerController;
+    protected bool isSpecialActionPressed = false;
+
+    public void Start()
+    {
+        
+    }
 
     public float Timer
     {
@@ -30,9 +39,10 @@ public class EvolutionComponent : MonoBehaviour {
         transform.GetChild((int)PlayerChildren.Evolutions).GetChild((int)evolution.BodyPart).gameObject.SetActive(true);
         isEvolutionInitialized = true;
         Timer = evolution.duration;
+        playerController = GetComponent<PlayerController>();
     }
 
-    void Update()
+    public virtual void Update()
     {
         if (isTimerInitialized)
         {
@@ -42,11 +52,27 @@ public class EvolutionComponent : MonoBehaviour {
                 Destroy(this);
             }
         }
+        if (playerController.IsUsingAController)
+        {
+            if (GamePad.GetState(playerController.playerIndex).Buttons.X == ButtonState.Pressed && !isSpecialActionPressed)
+            {
+                SpecialAction();
+                isSpecialActionPressed = true;
+            }
+            else if (GamePad.GetState(playerController.playerIndex).Buttons.X == ButtonState.Released)
+                isSpecialActionPressed = false;
+        }
     }
+    public virtual void SpecialAction()
+    {}
 
     private void OnDestroy()
     {
         GetComponent<Player>().activeEvolutions--;
         transform.GetChild((int)PlayerChildren.Evolutions).GetChild((int)evolution.BodyPart).gameObject.SetActive(false);
+    }
+    public virtual void OnCollisionEnter(Collision coll)
+    {
+
     }
 }
---
+++ Assets/Scripts/Evolutions/EvolutionStrengh.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+// utils 
+//GameManager.Instance.PlayerStart.PlayersReference[0].GetComponent<PlayerController>();
+//GamePad.GetState(playerIndex).Buttons.A;
+
+public class EvolutionStrengh : EvolutionComponent
+{
+    [SerializeField]float downDashPower = 100;
+    [SerializeField] float expulseDashPower = 60;
+    bool isDownDashing = false;
+    Rigidbody rb;
+
+
+    public void Start()
+    {
+        SetPower(Powers.Strengh);
+        rb = GetComponent<Rigidbody>();
+        Debug.Log("I am Strengh");
+    }
+  
+    public override void SpecialAction()
+    {
+        base.SpecialAction();
+     
+        JumpManager jumpManager = GetComponent<JumpManager>();
+        if (rb == null)
+        {
+            Debug.Log("Error : missing Rb on player");
+            return;
+        }
+        isDownDashing = true;
+        Vector3 downPush = Vector3.down * downDashPower;
+        rb.velocity = downPush; // Override current velocity. 
+        //rb.AddForce(downPush, ForceMode.Impulse); // doesn't overide current velocity;
+        if (jumpManager!=null)
+        {
+            jumpManager.Stop();
+        }
+    }
+    public override void OnCollisionEnter(Collision coll)
+    {
+        base.OnCollisionEnter(coll);
+        Rigidbody otherRb;
+        if (isDownDashing)
+        {
+            if (coll.transform.GetComponent<Player>() != null)
+            {
+                otherRb = coll.transform.GetComponent<Rigidbody>();
+                Vector3 direction = coll.contacts[0].point - rb.position;
+                direction.y = 0;
+                direction.Normalize();
+                otherRb.AddForce(direction * expulseDashPower, ForceMode.Impulse);
+            }
+        }
+        if (playerController.IsGrounded)
+        {
+            isDownDashing = false;
+        }
+    }
+}
