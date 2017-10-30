552561638074a69bf153e85834c0c1eb73c8b2e6
Unknown <Sebdami@hotmail.fr>
2017-10-30 17:34:23 +0100

    I commit

---
+++ Assets/GhostTrail.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostTrail : MonoBehaviour {
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.GetComponent<PlayerController>().enabled = false;
+        //Ultra temporary: Need to relieve restrictions when the player hits something
+        other.GetComponentInParent<Rigidbody>().velocity = other.transform.forward.normalized * other.GetComponent<PlayerController>().stats.Get(Stats.StatType.GROUND_SPEED);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.GetComponent<PlayerController>().enabled = true;
+    }
+}
---
+++ Assets/Scripts/Evolutions/EvolutionGhost.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvolutionGhost : EvolutionComponent
+{
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
