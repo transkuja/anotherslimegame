9f5bbdac090673bb656d43e7626685106efbea08
Unknown <olivier.serris@wanadoo.fr>
2017-11-20 19:19:14 +0100

    Log merde

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -103,7 +103,6 @@ public class PlayerController : MonoBehaviour
             {
                 ChangeDampingValuesCameraFreeLook(0);
 
-                Debug.Log("false");
                 
             }
 
