6bda00131d67ff3fd912aea73903a8bdd019b2f3
Unknown <transkuja@hotmail.fr>
2017-11-25 18:38:39 +0100

    Transfer things from new_ld_test to master + clean up

--- Assets/Scripts/WaterComponent.cs
+++ Assets/Scripts/WaterComponent.cs
@@ -7,6 +7,38 @@ public class WaterComponent : MonoBehaviour {
     public float bounceDamp;
     public float waterLevel;
     public float floatHeight;
+    public float compensationGravity;
+
+    StatBuff movestatbuff;
+    StatBuff dashstatbuff;
+    StatBuff jumpstatbuff;
+    public float tolerance;
+    public float waterResistance;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>() != null)
+        {
+            if(other.transform.childCount == 3 && other.transform.GetChild(2).GetComponent<ParticleRenderer>())
+                // SEB C'est pour toi
+            other.transform.GetChild(2).gameObject.SetActive(true);
+
+            if (waterResistance != 0)
+            {
+                movestatbuff = new StatBuff(Stats.StatType.GROUND_SPEED, waterResistance, -1);
+                other.GetComponent<PlayerController>().stats.AddBuff(movestatbuff);
+
+                dashstatbuff = new StatBuff(Stats.StatType.DASH_FORCE, waterResistance, -1);
+                other.GetComponent<PlayerController>().stats.AddBuff(dashstatbuff);
+
+
+                jumpstatbuff = new StatBuff(Stats.StatType.JUMP_HEIGHT, waterResistance, -1);
+                other.GetComponent<PlayerController>().stats.AddBuff(jumpstatbuff);
+            }
+
+
+        }
+    }
 
     private void OnTriggerStay(Collider other)
     {
@@ -14,16 +46,47 @@ public class WaterComponent : MonoBehaviour {
         if (other.GetComponent<Rigidbody>() != null)
         {
             Rigidbody rigidbody = other.GetComponent<Rigidbody>();
-            Vector3 actionPoint = transform.position + transform.TransformDirection(buoyancyCentreOffset);
-            float forceFactor = 1f - ((actionPoint.y - waterLevel) / floatHeight);
 
-            if (forceFactor > 0f)
+            // Niveau de floattabilté, fonction de la hauteur du joueur
+            Vector3 actionPoint = transform.position + transform.TransformDirection(buoyancyCentreOffset);
+
+            float forceFactor = 1f - ((actionPoint.y - waterLevel) / floatHeight);
+            Vector3 gravity = new Vector3(0, other.GetComponent<JumpManager>().GetGravity(), 0) * compensationGravity;
+            if (other.transform.position.y > waterLevel - tolerance)
             {
-                Vector3 uplift = -Physics.gravity * (forceFactor - rigidbody.velocity.y * bounceDamp);
-                rigidbody.AddForceAtPosition(uplift, actionPoint);
+                other.GetComponent<PlayerController>().IsGrounded = true;
+            } else
+            {
+                if (forceFactor > 0f)
+                {
+                    Vector3 uplift = gravity * ((forceFactor - rigidbody.velocity.y) * bounceDamp);
+
+
+                    rigidbody.AddForceAtPosition(uplift, actionPoint);
+                }
             }
+
+
+      
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+
+        if (other.transform.childCount == 3 && other.transform.GetChild(2).GetComponent<ParticleRenderer>())
+            // SEB C'est pour toi
+            other.transform.GetChild(2).gameObject.SetActive(false);
+
+
+        if (waterResistance != 0)
+        {
+            // TODO : Need a contains buff ?
+            other.GetComponent<PlayerController>().stats.RemoveBuff(movestatbuff);
+            other.GetComponent<PlayerController>().stats.RemoveBuff(dashstatbuff);
+            other.GetComponent<PlayerController>().stats.RemoveBuff(jumpstatbuff);
         }
     }
-    
-
 }
