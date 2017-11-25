0f8b79bfbabd173666f6cd770c230bf0a040ddb2
Unknown <transkuja@hotmail.fr>
2017-11-26 00:54:57 +0100

    Continue LD

--- Assets/Scripts/Resource/PrefabIle.cs
+++ Assets/Scripts/Resource/PrefabIle.cs
@@ -5,15 +5,37 @@ using Random = UnityEngine.Random;
 
 public class PrefabIle : MonoBehaviour
 {
+    [Header("Rune shelters")]
+    public GameObject ghostRuneShelter;
+    public GameObject platformistRuneShelter;
+    public GameObject agileRuneShelter;
+    public GameObject strengthRuneShelter;
 
-    [SerializeField]
+    [Header("Islands")]
     public GameObject prefabIle1GameObject;
-
 
     public GameObject SpawnIleInstance(Vector3 where, Quaternion direction, Transform parent, bool useAlternativePrefab = false)
     {
         // USe random
         return Instantiate(prefabIle1GameObject, where, direction, parent);
 
+    }
+
+    public GameObject SpawnRuneShelterInstance(Vector3 where, Quaternion direction, Transform parent, CollectableType myItemType)
+    {
+        switch (myItemType)
+        {
+            case CollectableType.StrengthEvolution1:
+                return Instantiate(strengthRuneShelter, where, direction, parent);
+            case CollectableType.PlatformistEvolution1:
+                return Instantiate(platformistRuneShelter, where, direction, parent);
+            case CollectableType.AgileEvolution1:
+                return Instantiate(agileRuneShelter, where, direction, parent);
+            case CollectableType.GhostEvolution1:
+                return Instantiate(ghostRuneShelter, where, direction, parent);
+            default:
+                Debug.Log("Unknown Item type");
+                return null;
+        }
     }
 }
