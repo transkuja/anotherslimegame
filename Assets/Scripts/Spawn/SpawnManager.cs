3c48e2e05b4e235ccbebe9abe7ff8b73f6efa711
Unknown <remi.boscher@gmail.com>
2017-11-26 18:06:36 +0100

    Strength

--- Assets/Scripts/Spawn/SpawnManager.cs
+++ Assets/Scripts/Spawn/SpawnManager.cs
@@ -344,7 +344,6 @@ public class SpawnManager : MonoBehaviour{
         Utils.Shuffle(gameplayRoomStarters);
         foreach (Tampax tmp in gameplayRoomStarters)
         {
-            Debug.Log(tmp.gameplayRoomStarter);
             if(tmp.gameplayRoomStarter != null)
             {
                 if (tmp.evolutionType == evolutionType)
