2c8c0a5215f598613f4b6aa7dc1dd9dd2d85bcb4
Unknown <transkuja@hotmail.fr>
2017-11-27 10:40:46 +0100

    Create spawner for gameplay platform around/in main tower

--- Assets/Scripts/Resource/MainTowerGameplayManager.cs
+++ Assets/Scripts/Resource/MainTowerGameplayManager.cs
@@ -4,6 +4,51 @@ using UnityEngine;
 
 public class MainTowerGameplayManager : MonoBehaviour {
 
-    public List<GameObject> prefabsGameplayPlateforms;
+    [Tooltip("The transform from which all instantiates will be done. Instantiated platforms will be this transform's children.")]
+    public Transform referenceTransform;
 
+    public List<GameObject> prefabsGameplayPlateformsExterior;
+    public List<GameObject> prefabsGameplayPlateformsInterior;
+
+    public int initialHeight = 30;
+    public int heightStep = 10;
+    public int necessaryFloorsExt = 4;
+    public int necessaryFloorsInt = 2;
+    
+    private void Start()
+    {
+        if (referenceTransform == null)
+        {
+            Debug.LogWarning("No reference transform specified. Won't spawn anything.");
+            return;
+        }
+
+        if (prefabsGameplayPlateformsExterior == null || prefabsGameplayPlateformsExterior.Count == 0)
+        {
+            Debug.LogWarning("No prefabs in list for gameplay outside the main tower.");
+        }
+        else
+        {
+            for (int i = 0; i < necessaryFloorsExt; i++)
+            {
+                GameObject go = Instantiate(prefabsGameplayPlateformsExterior[Random.Range(0, prefabsGameplayPlateformsExterior.Count)], referenceTransform);
+                go.transform.localPosition = new Vector3(0, initialHeight + i * heightStep, 0);
+                go.transform.localRotation = Quaternion.identity;
+            }
+        }
+
+        if (prefabsGameplayPlateformsInterior == null || prefabsGameplayPlateformsInterior.Count == 0)
+        {
+            Debug.LogWarning("No prefabs in list for gameplay inside the main tower.");
+        }
+        else
+        {
+            for (int i = 0; i < necessaryFloorsInt; i++)
+            {
+                GameObject go = Instantiate(prefabsGameplayPlateformsInterior[Random.Range(0, prefabsGameplayPlateformsInterior.Count)], referenceTransform);
+                go.transform.localPosition = new Vector3(0, initialHeight + i * heightStep, 0);
+                go.transform.localRotation = Quaternion.identity;
+            }
+        }
+    }
 }
