af254dc2857421baaa64db69c9666cc1a8c81f4b
Unknown <olivier.serris@wanadoo.fr>
2017-11-27 16:27:51 +0100

    Added MonsterShooter RUF

---
+++ Assets/Scripts/Monster/Bullet.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    Vector3 direction;
+    float speed;
+    bool isLaunched;
+    float bulletDistance;
+    float timerDist;
+    public void Init()
+    {
+        isLaunched = false;
+    }
+    public void Lauch(Vector3 _direction,float _speed,float _bulletDistance)
+    {
+        isLaunched = true;
+        direction = _direction;
+        speed = _speed;
+        bulletDistance = _bulletDistance;
+        timerDist = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isLaunched)
+        {
+            transform.position += direction * speed * Time.deltaTime;
+            timerDist += speed * Time.deltaTime;
+            if (timerDist> bulletDistance)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerCollisionCenter playerCollision = other.GetComponent<PlayerCollisionCenter>();
+        if (playerCollision != null)
+        {
+            playerCollision.DamagePlayer(other.GetComponent<Player>());
+            playerCollision.ExpulsePlayer(other.ClosestPoint(transform.position), other.GetComponent<Rigidbody>(), 50);
+        }
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Monster/MonsterShooter.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MonsterShooter : MonoBehaviour {
+
+    // temp : bulletPrefab
+    [SerializeField] GameObject bulletPrefab;
+
+    public delegate void action();
+    action CurAction;
+
+    [SerializeField] private int bulletPerRafales;
+    [SerializeField] private float chargeDelay;
+    [SerializeField] private float rafalesChargeDelay;
+    [SerializeField] private float bulletDistance;
+    [SerializeField] private float bulletSpeed;
+
+
+
+    private float timer;
+    private float curBullet;
+    Transform bulletSpawn;
+
+    void OnBegin()
+    {
+        timer = 0;
+        CurAction = ChargeWait;
+    }
+
+    void ChargeWait()
+    {
+        timer += Time.deltaTime;
+        if (timer > chargeDelay)
+        {
+            timer = 0;
+            CurAction = Shoot;
+        }
+    }
+    void RafaleWait()
+    {
+        timer += Time.deltaTime;
+        if (timer > rafalesChargeDelay)
+        {
+            timer = 0;
+            CurAction = Shoot;
+        }
+    }
+  
+    void Shoot()
+    {
+        curBullet++;
+        CreateBullet();
+        if (curBullet >= bulletPerRafales)
+        {
+            curBullet = 0;
+            CurAction = RafaleWait;
+        }
+        else
+        {
+            CurAction = ChargeWait;
+        }
+    }
+    public void CreateBullet()
+    {
+        GameObject bulletGo = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity,transform);
+        Bullet bullet = bulletGo.GetComponent<Bullet>();
+        bullet.Init();
+        bullet.Lauch(transform.forward, bulletSpeed, bulletDistance);
+    }
+    // Use this for initialization
+    private void Awake()
+    {
+        bulletSpawn = transform.Find("BulletSpawn");
+    }
+    void Start () {
+        OnBegin();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (CurAction!=null)
+            CurAction();
+    }
+}
