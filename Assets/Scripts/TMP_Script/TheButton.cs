2cc8c265c21b428980065e709fd6f9917bd1f99b
Unknown <remi.boscher@gmail.com>
2017-11-29 02:31:37 +0100

    Having fun with water

---
+++ Assets/Scripts/TMP_Script/TheButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheButton : MonoBehaviour {
+
+    public GameObject water;
+
+    [Range(0.1f, 0.5f)]
+    public float speed = 0.5f;
+
+    [Header("height + water.position")]
+    public float heightToReach = 32;
+
+    private bool moveWater = false;
+    private Vector3 positionToReach;
+
+    public void Start()
+    {
+
+        positionToReach = water.transform.position + heightToReach * Vector3.up;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Player>())
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            GetComponent<Animator>().SetBool("test", true);
+            moveWater = true;
+        }
+    }
+
+    public void Update()
+    {
+        if (moveWater)
+            water.transform.position = Vector3.Lerp(water.transform.position, positionToReach, speed * Time.deltaTime );
+    }
+}
