0200a5aaad4a5d58387a5731923f8a2ba94d118a
Unknown <remi.boscher@gmail.com>
2017-11-29 02:38:43 +0100

    LOL

--- Assets/Scripts/TMP_Script/TheButton.cs
+++ Assets/Scripts/TMP_Script/TheButton.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UWPAndXInput;
 using UnityEngine;
 
 public class TheButton : MonoBehaviour {
@@ -17,7 +18,6 @@ public class TheButton : MonoBehaviour {
 
     public void Start()
     {
-
         positionToReach = water.transform.position + heightToReach * Vector3.up;
     }
 
@@ -28,12 +28,23 @@ public class TheButton : MonoBehaviour {
             GetComponent<BoxCollider>().enabled = false;
             GetComponent<Animator>().SetBool("test", true);
             moveWater = true;
+            for (int i = 0; i < GameManager.Instance.PlayerStart.PlayersReference.Count; i++){
+                GamePad.SetVibration((PlayerIndex)i, 1, 1);
+            }
         }
     }
 
     public void Update()
     {
         if (moveWater)
-            water.transform.position = Vector3.Lerp(water.transform.position, positionToReach, speed * Time.deltaTime );
+        {
+            water.transform.position = Vector3.Lerp(water.transform.position, positionToReach, speed * Time.deltaTime);
+            if(  Vector3.Distance (water.transform.position, positionToReach) < 0.1f)
+            {
+                moveWater = false;
+            }
+        }
+
+       
     }
 }
