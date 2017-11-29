f8314e55e121836435fa6806f7d1b214f4231b7c
Unknown <berthetmathieu@free.fr>
2017-11-29 18:06:14 +0100

    Continue indication Texte (2)

--- Assets/Scripts/Asuppr.cs
+++ Assets/Scripts/Asuppr.cs
@@ -12,23 +12,38 @@ public class Asuppr : MonoBehaviour
 
         for (int i = 0; i < GameManager.Instance.PlayerStart.PlayersReference.Count; i++)
         {
-            GameManager.Instance.PlayerStart.PlayersReference[i].transform.GetChild(3).GetChild(i).GetComponentInChildren<Text>().gameObject.transform.LookAt(transform);
-            
-        //player.GetComponentInChildren<Text>().gameObject.transform.parent.LookAt(transform);
+            if(GameManager.Instance.PlayerStart.PlayersReference[i].GetComponent<Renderer>().isVisible)
+            {
+                GameManager.Instance.PlayerStart.PlayersReference[i].transform.GetChild(3).GetChild(i).GetComponentInChildren<Text>().gameObject.transform.LookAt(transform);
+            }
+            else
+            {
+                GameManager.Instance.PlayerStart.PlayersReference[i].transform.GetChild((int)PlayerChildren.Canvas).GetChild(i).gameObject.SetActive(false);
 
-        //player.transform.GetChild(3).GetComponentInChildren<Text>().gameObject.transform.LookAt(transform.parent.GetComponentInChildren<Cinemachine.CinemachineFreeLook>().transform);
-        /*GameObject objec = player.transform.GetChild(2).GetComponentInChildren<Text>().gameObject.transform.gameObject;
-        Debug.Log("");*/
+            }
+            //player.GetComponentInChildren<Text>().gameObject.transform.parent.LookAt(transform);
+
+            //player.transform.GetChild(3).GetComponentInChildren<Text>().gameObject.transform.LookAt(transform.parent.GetComponentInChildren<Cinemachine.CinemachineFreeLook>().transform);
+            /*GameObject objec = player.transform.GetChild(2).GetComponentInChildren<Text>().gameObject.transform.gameObject;
+            Debug.Log("");*/
         }
     }
     private void OnPostRender()
     {
         for (int i = 0; i < GameManager.Instance.PlayerStart.PlayersReference.Count; i++)
         {
-            GameManager.Instance.PlayerStart.PlayersReference[i].transform.GetChild(3).GetChild(i).GetComponentInChildren<Text>().gameObject.transform.LookAt(transform);
+            if (GameManager.Instance.PlayerStart.PlayersReference[i].GetComponent<Renderer>().isVisible)
+            {
+                GameManager.Instance.PlayerStart.PlayersReference[i].transform.GetChild(3).GetChild(i).GetComponentInChildren<Text>().gameObject.transform.LookAt(transform);
+            }
+            else
+            {
+                GameManager.Instance.PlayerStart.PlayersReference[i].transform.GetChild((int)PlayerChildren.Canvas).GetChild(i).gameObject.SetActive(false);
 
+            }
             //GameObject player = GameManager.Instance.PlayerStart.PlayersReference[i];
             //player.GetComponentInChildren<Text>().gameObject.transform.parent.LookAt(transform);
+
 
             /*player.transform.GetChild(3).GetComponentInChildren<Text>().gameObject.transform.LookAt(transform.parent.GetComponentInChildren<Cinemachine.CinemachineFreeLook>().transform);
             GameObject objec = player.transform.GetChild(2).GetComponentInChildren<Text>().gameObject.transform.gameObject;
