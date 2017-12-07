eeaf5f0974ac84d3937ad55d5caf01bdc945878d
Unknown <transkuja@hotmail.fr>
2017-12-07 13:36:58 +0100

    Start working on breakables (but it's coffee time)

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        PlayerController player = collision.transform.GetComponent<PlayerController>();
+        if (player != null && (
+                player.PlayerState == player.dashState
+                || player.PlayerState == player.downDashState
+                )
+           )
+        {
+            // impact
+            // pool de morceaux cassés
+        }
+    }
+}
---
+++ Assets/Scripts/Resource/PoolManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour {
+
+    public List<GameObject> pieces;
+    public int poolSize = 50;
+    public List<GameObject> pool;
+
+	// Use this for initialization
+	void Start () {
+        GameObject poolParent = new GameObject("BreakablePiecesPool");
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject piece = Instantiate(pieces[Random.Range(0, pieces.Count)], poolParent.transform);
+            piece.SetActive(false);
+            pool.Add(piece);
+        }
+	}
+
+}
