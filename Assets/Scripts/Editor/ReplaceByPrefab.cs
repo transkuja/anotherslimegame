07d265f00d3b23c59d329d6afd73ed55f04b171b
Unknown <olivier.serris@wanadoo.fr>
2017-12-11 15:52:33 +0100

    Added StrenghRoom with Wall As prefab( waiting for texture for full implementation

---
+++ Assets/Scripts/Editor/ReplaceByPrefab.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ReplaceByPrefab : EditorWindow {
+
+    string myString = "Hello World";
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+    GameObject prefabModel;
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Window/ReplacePrefab")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        ReplaceByPrefab window = (ReplaceByPrefab)EditorWindow.GetWindow(typeof(ReplaceByPrefab));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Replace all selected scene gameobject by \n this prefab model :", EditorStyles.boldLabel);
+        prefabModel =(GameObject) EditorGUILayout.ObjectField(prefabModel,typeof(GameObject),true);
+        if (GUILayout.Button("Replace all (DESTRUCTIVE ACTION)"))
+        {
+            if (prefabModel == null)
+            {
+                return;
+            }
+            else
+                ReplaceAll();
+        }
+    }
+    void ReplaceAll()
+    {
+        GameObject[] selectedGo = Selection.gameObjects;
+        foreach(GameObject go in selectedGo)
+        {
+            //go.SetActive(false);
+            Instantiate(prefabModel, go.transform.position, go.transform.rotation, go.transform.parent);
+            DestroyImmediate(go);
+        }
+    }
+}
