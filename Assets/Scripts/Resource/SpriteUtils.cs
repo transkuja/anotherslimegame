f5a2305d6ce4f58900bde9553a41427afb11d7ac
Unknown <transkuja@hotmail.fr>
2017-12-11 18:02:29 +0100

    Add sprite utils

---
+++ Assets/Scripts/Resource/SpriteUtils.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteUtils : MonoBehaviour {
+
+    [Header("Victory panel")]
+    [SerializeField]
+    Sprite victoryPanel_1;
+    [SerializeField]
+    Sprite victoryPanel_2;
+    [SerializeField]
+    Sprite victoryPanel_3;
+    [SerializeField]
+    Sprite victoryPanel_4;
+    [SerializeField]
+    Sprite victoryNumber1;
+    [SerializeField]
+    Sprite victoryNumber2;
+    [SerializeField]
+    Sprite victoryNumber3;
+    [SerializeField]
+    Sprite victoryNumber4;
+
+}
