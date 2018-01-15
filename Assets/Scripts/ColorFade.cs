0656e699fe1aeecb7f9f478c7d0184f56860395f
Unknown <Sebdami@hotmail.fr>
2018-01-15 18:01:40 +0100

    Add new player shader (faces and colors!)

---
+++ Assets/Scripts/ColorFade.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorFade : MonoBehaviour {
+    Material m;
+    [SerializeField]
+    float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+        m = GetComponentInChildren<MeshRenderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h, s, v;
+
+        Color.RGBToHSV(m.color, out h, out s, out v);
+
+        h += Time.deltaTime * speed;
+
+        m.color = Color.HSVToRGB(h, s, v);
+	}
+}
