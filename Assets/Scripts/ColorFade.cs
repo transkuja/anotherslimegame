9040459fd6953138715db9dd158916b6975f5b37
Unknown <Sebdami@hotmail.fr>
2018-01-16 13:23:47 +0100

    Add genericity to color fading

--- Assets/Scripts/ColorFade.cs
+++ Assets/Scripts/ColorFade.cs
@@ -3,22 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ColorFade : MonoBehaviour {
-    Material m;
+    List<Material> mats;
     [SerializeField]
     float speed = 0.5f;
 	// Use this for initialization
 	void Start () {
-        m = GetComponentInChildren<MeshRenderer>().material;
+        Renderer[] mr = GetComponentsInChildren<Renderer>();
+        mats = new List<Material>();
+        for(int i = 0; i < mr.Length; i++)
+        {
+            Material[] ms = mr[i].materials;
+            for(int j = 0; j < ms.Length; j++)
+            {
+                mats.Add(ms[j]);
+                mats[mats.Count-1].color = Color.red;
+            }
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float h, s, v;
+        foreach (Material m in mats)
+        {
+            Color.RGBToHSV(m.color, out h, out s, out v);
 
-        Color.RGBToHSV(m.color, out h, out s, out v);
+            h += Time.deltaTime * speed;
 
-        h += Time.deltaTime * speed;
-
-        m.color = Color.HSVToRGB(h, s, v);
+            m.color = Color.HSVToRGB(h, s, v);
+        }
 	}
 }
