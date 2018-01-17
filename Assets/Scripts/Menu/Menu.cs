5036ec26fef9e4b7bedca2a4e33722b37925da69
Unknown <transkuja@hotmail.fr>
2018-01-17 23:46:54 +0100

    Add character customization

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -21,7 +21,17 @@ public class Menu : MonoBehaviour {
 
     public GameObject playerCustomScreenPrefab;
 
-    List<GameObject> playerCustomScreens;
+    List<GameObject> playerCustomScreens = new List<GameObject>();
+
+    public List<Color> customColors;
+
+    GamePadState[] prevControllerStates = new GamePadState[4];
+    GamePadState[] controllerStates = new GamePadState[4];
+
+    int[] selectedColors = new int[4];
+    int[] selectedFaces = new int[4];
+    int maxFacesNumber = 4;
+    int[] currentCursorsRow = new int[4];
 
     public void SetMode(int _modeSelected)
     {
@@ -36,69 +46,140 @@ public class Menu : MonoBehaviour {
     private void Start()
     {
         SetState(MenuState.TitleScreen);
+        //for (int i = 0; i < 4; i++)
+        //{
+        //    prevControllerStates.Add(GamePad.GetState((PlayerIndex)i));
+        //    controllerStates.Add(GamePad.GetState((PlayerIndex)i));
+        //}
     }
 
     void Update()
     {
-        prevControllerState = controllerState;
-        controllerState = GamePad.GetState(0);
-
-        if (Input.anyKey && currentState == MenuState.TitleScreen)
+        if (currentState != MenuState.CustomisationScreen)
         {
-            GoToNextState();
-        }
+            prevControllerState = controllerState;
+            controllerState = GamePad.GetState(0);
 
-
-        if ((controllerState.ThumbSticks.Left.X > 0.5f && prevControllerState.ThumbSticks.Left.X < 0.5f)
-                || (controllerState.ThumbSticks.Left.Y < -0.75f && prevControllerState.ThumbSticks.Left.Y > -0.75f))
-        {
-            buttonNeedUpdate = true;
-            currentCursor++;
-        }
-        else if ((controllerState.ThumbSticks.Left.X < -0.5f && prevControllerState.ThumbSticks.Left.X > -0.5f)
-                || (controllerState.ThumbSticks.Left.Y > 0.75f && prevControllerState.ThumbSticks.Left.Y < 0.75f))
-        {
-            buttonNeedUpdate = true;
-            currentCursor--;
-        }
-
-        // Update visual feedback
-        if (buttonNeedUpdate)
-        {
-            if (currentState == MenuState.NumberOfPlayers && selectedMode == 1)
+            if (Input.anyKey && currentState == MenuState.TitleScreen)
             {
-                if (currentCursor < 0)
-                    currentCursor = 3;
-                else
-                    currentCursor = currentCursor % 4;
-                currentlySelectedButton = transform.GetChild((int)currentState).GetChild(3).GetChild(currentCursor).GetComponent<Button>();
-                currentlySelectedButton.Select();
-            }
-            else
-            {
-                if (currentCursor < 0)
-                    currentCursor = 1;
-                else
-                    currentCursor = currentCursor % 2;
-                currentlySelectedButton = transform.GetChild((int)currentState).GetChild(2).GetChild(currentCursor).GetComponent<Button>();
-                currentlySelectedButton.Select();
-            }
-            buttonNeedUpdate = false;
-        }
-
-        if (prevControllerState.Buttons.A == ButtonState.Released && controllerState.Buttons.A == ButtonState.Pressed)
-        {
-            if (currentlySelectedButton != null)
-            {
-                currentlySelectedButton.onClick.Invoke();
                 GoToNextState();
             }
-        }
 
-        else if (prevControllerState.Buttons.B == ButtonState.Released && controllerState.Buttons.B == ButtonState.Pressed)
+            if ((controllerState.ThumbSticks.Left.X > 0.5f && prevControllerState.ThumbSticks.Left.X < 0.5f)
+                    || (controllerState.ThumbSticks.Left.Y < -0.75f && prevControllerState.ThumbSticks.Left.Y > -0.75f))
+            {
+                buttonNeedUpdate = true;
+                currentCursor++;
+            }
+            else if ((controllerState.ThumbSticks.Left.X < -0.5f && prevControllerState.ThumbSticks.Left.X > -0.5f)
+                    || (controllerState.ThumbSticks.Left.Y > 0.75f && prevControllerState.ThumbSticks.Left.Y < 0.75f))
+            {
+                buttonNeedUpdate = true;
+                currentCursor--;
+            }
+        
+
+            // Update visual feedback
+            if (buttonNeedUpdate)
+            {
+                if (currentState == MenuState.NumberOfPlayers && selectedMode == 1)
+                {
+                    if (currentCursor < 0)
+                        currentCursor = 3;
+                    else
+                        currentCursor = currentCursor % 4;
+                    currentlySelectedButton = transform.GetChild((int)currentState).GetChild(3).GetChild(currentCursor).GetComponent<Button>();
+                    currentlySelectedButton.Select();
+                }
+                else
+                {
+                    if (currentCursor < 0)
+                        currentCursor = 1;
+                    else
+                        currentCursor = currentCursor % 2;
+                    currentlySelectedButton = transform.GetChild((int)currentState).GetChild(2).GetChild(currentCursor).GetComponent<Button>();
+                    currentlySelectedButton.Select();
+                }
+                buttonNeedUpdate = false;
+            }
+
+            if (prevControllerState.Buttons.A == ButtonState.Released && controllerState.Buttons.A == ButtonState.Pressed)
+            {
+                if (currentlySelectedButton != null)
+                {
+                    currentlySelectedButton.onClick.Invoke();
+                    GoToNextState();
+                }
+            }
+
+            else if (prevControllerState.Buttons.B == ButtonState.Released && controllerState.Buttons.B == ButtonState.Pressed)
+            {
+                if (currentState != MenuState.TitleScreen)
+                    ReturnToPreviousState();
+            }
+        }
+        else
         {
-            if (currentState != MenuState.TitleScreen)
-                ReturnToPreviousState();
+            if (nbPlayers == -1)
+                return;
+
+            for (int i = 0; i < nbPlayers; i++)
+            {
+                prevControllerStates[i] = controllerStates[i];
+                controllerStates[i] = GamePad.GetState((PlayerIndex)i);
+
+                if (controllerStates[i].ThumbSticks.Left.Y > 0.5f && prevControllerStates[i].ThumbSticks.Left.Y < 0.5f
+                    || (controllerState.ThumbSticks.Left.Y < -0.5f && prevControllerState.ThumbSticks.Left.Y > -0.5f))
+                {
+                    currentCursorsRow[i]++;
+                    currentCursorsRow[i] = currentCursorsRow[i] % 2;
+                }
+                else if (controllerStates[i].ThumbSticks.Left.X > 0.5f && prevControllerStates[i].ThumbSticks.Left.X < 0.5f)
+                {
+                    if (currentCursorsRow[i] == 0)
+                    {
+                        selectedColors[i]++;
+                        selectedColors[i] = selectedColors[i] % customColors.Count;
+                        // Update text and character
+                        playerCustomScreens[i].transform.GetChild(1).GetComponent<Text>().text = (selectedColors[i]+1).ToString();
+                        playerCustomScreens[i].transform.GetChild(3).GetComponentInChildren<PlayerCosmetics>().SetUniqueColor(customColors[selectedColors[i]]);
+                    }
+                    else
+                    {
+                        selectedFaces[i]++;
+                        selectedFaces[i] = selectedFaces[i] % maxFacesNumber;
+                        // Update text and character
+                        playerCustomScreens[i].transform.GetChild(2).GetComponent<Text>().text = (selectedFaces[i]+1).ToString();
+                        playerCustomScreens[i].transform.GetChild(3).GetComponentInChildren<PlayerCosmetics>().FaceType = (FaceType)selectedFaces[i];
+                    }
+                }
+                else if (controllerState.ThumbSticks.Left.X < -0.5f && prevControllerState.ThumbSticks.Left.X > -0.5f)
+                {
+                    if (currentCursorsRow[i] == 0)
+                    {
+                        selectedColors[i]--;
+                        if (selectedColors[i] < 0)
+                            selectedColors[i] = customColors.Count - 1;
+                        else
+                            selectedColors[i] = selectedColors[i] % customColors.Count;
+                        // Update text and character
+                        playerCustomScreens[i].transform.GetChild(1).GetComponent<Text>().text = (selectedColors[i]+1).ToString();
+                        playerCustomScreens[i].transform.GetChild(3).GetComponentInChildren<PlayerCosmetics>().SetUniqueColor(customColors[selectedColors[i]]);
+                    }
+                    else
+                    {
+                        selectedFaces[i]--;
+                        if (selectedFaces[i] < 0)
+                            selectedFaces[i] = maxFacesNumber - 1;
+                        else
+                            selectedFaces[i] = selectedFaces[i] % maxFacesNumber;
+
+                        // Update text and character
+                        playerCustomScreens[i].transform.GetChild(2).GetComponent<Text>().text = (selectedFaces[i]+1).ToString();
+                        playerCustomScreens[i].transform.GetChild(3).GetComponentInChildren<PlayerCosmetics>().FaceType = (FaceType)selectedFaces[i];
+                    }
+                }
+            }
         }
     }
 
@@ -113,17 +194,21 @@ public class Menu : MonoBehaviour {
 
         if (currentState == MenuState.CustomisationScreen)
         {
+            playerCustomScreens.Clear();
             for (int i = 0; i < nbPlayers; i++)
             {
                 GameObject go = Instantiate(playerCustomScreenPrefab, transform.GetChild((int)MenuState.CustomisationScreen));
-                if (nbPlayers%2 == 0)
-                {
-                    go.transform.localPosition = new Vector3(i* -(Screen.width / 4), 0.0f, 0.0f);
-                }
-                else
-                {
-                    //go.transform.localPosition = new Vector3(i * + (-1-(Screen.width / 4), 0.0f, 0.0f);
-                }
+                go.GetComponentInChildren<Text>().text = "Player " + (i + 1);
+
+                if (nbPlayers == 1)
+                    go.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+                if (nbPlayers == 2)
+                    go.transform.localPosition = new Vector3(-(250) + (2*i) * (250), 0.0f, 0.0f);
+                if (nbPlayers == 3)
+                    go.transform.localPosition = new Vector3((-(250) + i *(250)), 0.0f, 0.0f);
+                if (nbPlayers == 4)
+                    go.transform.localPosition = new Vector3(-(300) + (i * (200)), 0.0f, 0.0f);
+
                 playerCustomScreens.Add(go);
             }
         }
