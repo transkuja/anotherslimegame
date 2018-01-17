4b7156a9ef250590b418765cf0a749547e4b8668
Unknown <olivier.serris@wanadoo.fr>
2018-01-17 16:47:06 +0100

    Template PlayerState

---
+++ Assets/Scripts/PlayerState/PlayerState2D.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState2D : PlayerState<PlayerController2d>
+{
+
+    public PlayerState2D(PlayerController2d _playerController)
+    {
+    }
+
+}
