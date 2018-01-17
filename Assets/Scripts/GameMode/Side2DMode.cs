d40b7efcedad2f8bc0754bd33d012994b201d5ef
Unknown <olivier.serris@wanadoo.fr>
2018-01-17 17:37:23 +0100

    2DMode Empty

---
+++ Assets/Scripts/GameMode/Side2DMode.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Side2DMode : GameMode {
+
+    public override void AttributeCamera(uint activePlayersAtStart, GameObject[] cameraReferences, List<GameObject> playersReference)
+    {
+    }
+}
