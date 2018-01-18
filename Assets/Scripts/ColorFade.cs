e1444e8a377380193e41d04ad3f5efaed2a04fcc
Unknown <Sebdami@hotmail.fr>
2018-01-18 18:23:55 +0100

    Add relativity to color fading
     (uncheck it  on all the parts to get the old colors back)

--- Assets/Scripts/ColorFade.cs
+++ Assets/Scripts/ColorFade.cs
@@ -9,22 +9,36 @@ public class ColorFade : MonoBehaviour {
 
     [SerializeField]
     bool randomSpeed = true;
-	// Use this for initialization
-	void Start () {
-        if(randomSpeed)
+
+    [SerializeField]
+    bool isColorRelativeToPositionOnStart = false;
+    [SerializeField]
+    float positionRelativityMultiplier = 0.003f;
+    // Use this for initialization
+    void Start()
+    {
+        if (randomSpeed)
             speed = Random.Range(0.2f, 0.8f);
         Renderer[] mr = GetComponentsInChildren<Renderer>();
         mats = new List<Material>();
-        for(int i = 0; i < mr.Length; i++)
+        for (int i = 0; i < mr.Length; i++)
         {
             Material[] ms = mr[i].materials;
-            for(int j = 0; j < ms.Length; j++)
+            for (int j = 0; j < ms.Length; j++)
             {
                 mats.Add(ms[j]);
-                mats[mats.Count-1].color = Color.red;
+                mats[mats.Count - 1].color = Color.red;
+                if(isColorRelativeToPositionOnStart && GetComponent<MeshFilter>() != null)
+                {
+                    float h, s, v;
+                    Color.RGBToHSV(mats[mats.Count - 1].color, out h, out s, out v);
+                    h += GetComponent<MeshFilter>().sharedMesh.bounds.center.magnitude * positionRelativityMultiplier;
+                    mats[mats.Count - 1].color = Color.HSVToRGB(h, s, v);
+                }
+                
             }
         }
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
