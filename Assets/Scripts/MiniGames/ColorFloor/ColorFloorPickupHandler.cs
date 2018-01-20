e4a863abaf03065a05947c11c339c44627deb771
Unknown <transkuja@hotmail.fr>
2018-01-20 11:43:23 +0100

    Create pickup spawner

---
+++ Assets/Scripts/MiniGames/ColorFloor/ColorFloorPickupHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorFloorPickupHandler : MonoBehaviour {
+
+    [SerializeField]
+    float pickupDelay = 0.5f;
+    [SerializeField]
+    int maxPickupOnMap = 3;
+
+    int pickupSpawned = 0;
+
+    IEnumerator Start () {
+		while (true)
+        {
+            yield return new WaitForSeconds(pickupDelay);
+            if (pickupSpawned < maxPickupOnMap)
+            {
+                // spawn pickup
+                pickupSpawned++;
+            }
+        }
+	}
+
+}
