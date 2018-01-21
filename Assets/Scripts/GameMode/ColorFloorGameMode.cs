4ae8e269841cc9f4196ad29c776973991674d82c
Unknown <transkuja@hotmail.fr>
2018-01-21 14:03:56 +0100

    Create gamemode for colorfloor and add timer + score screen

---
+++ Assets/Scripts/GameMode/ColorFloorGameMode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UWPAndXInput;
+public class ColorFloorGameMode : GameMode {
+
+    public float timer;
+
+    public override void StartGame(List<GameObject> playerReferences)
+    {
+        base.StartGame(playerReferences);
+
+        LaunchTimer();
+    }
+
+    public void LaunchTimer()
+    {
+        GameManager.Instance.GameFinalTimer = timer;
+        GameManager.Instance.LaunchFinalTimer();
+    }
+
+    public override void AttributeCamera(uint activePlayersAtStart, GameObject[] cameraReferences, List<GameObject> playersReference)
+    {
+        base.AttributeCamera(activePlayersAtStart, cameraReferences, playersReference);
+        for (int i = 0; i < activePlayersAtStart; i++)
+        {
+            GameObject go = playersReference[i];
+
+            go.GetComponent<Player>().cameraReference = cameraReferences[i];
+            cameraReferences[i].SetActive(true);
+
+        }
+    }
+
+    public override void PlayerHasFinished(Player player)
+    {
+        GameManager.Instance.ScoreScreenReference.RankPlayersByPoints();
+    }
+}
