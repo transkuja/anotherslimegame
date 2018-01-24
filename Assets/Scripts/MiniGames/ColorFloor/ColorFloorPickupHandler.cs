0dcc97829ca95540c3b8f746a2536a95d72f4c44
Unknown <transkuja@hotmail.fr>
2018-01-24 14:48:14 +0100

    ColorFloor: Pickup spawned based on new LD

--- Assets/Scripts/MiniGames/ColorFloor/ColorFloorPickupHandler.cs
+++ Assets/Scripts/MiniGames/ColorFloor/ColorFloorPickupHandler.cs
@@ -12,20 +12,34 @@ public class ColorFloorPickupHandler : MonoBehaviour {
     [HideInInspector]
     public int pickupSpawned = 0;
 
+    int mapSize = 0;
+    int lineCount;
+    int lineSize;
+
     IEnumerator Start () {
+        lineCount = transform.childCount;
+        for (int i = 0; i < transform.childCount; i++)
+            mapSize += transform.GetChild(i).childCount;
+        
 		while (true)
         {
             yield return new WaitForSeconds(pickupDelay);
             if (pickupSpawned < maxPickupOnMap)
             {
                 // Spawn pickup
-                int randChild = Random.Range(0, transform.childCount);
+                int randChild = Random.Range(0, mapSize);
+                lineSize = transform.GetChild(randChild / lineCount).childCount;
 
                 // Makes sure we don't spawn twice at the same place
-                while (transform.GetChild(randChild).childCount > 0)
-                    randChild = Random.Range(0, transform.childCount);
+                while (transform.GetChild(randChild/lineCount).GetChild(randChild%lineSize).childCount > 0)
+                    randChild = Random.Range(0, mapSize);
 
-                ResourceUtils.Instance.poolManager.colorFloorScorePickUpPool.GetItem(transform.GetChild(randChild), Vector3.up, Quaternion.identity, true);
+                ResourceUtils.Instance.poolManager.colorFloorScorePickUpPool.GetItem(
+                    transform.GetChild(randChild / lineCount).GetChild(randChild % lineSize),
+                    Vector3.up, 
+                    Quaternion.identity,
+                    true
+                );
 
                 pickupSpawned++;
             }
