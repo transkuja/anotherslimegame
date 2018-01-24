7f87ecae5b47a5f3f8023cac3b547274f76db460
Unknown <Sebdami@hotmail.fr>
2018-01-24 17:47:03 +0100

    Kart: Start adding boost

--- Assets/Scripts/Player/PlayerControllerKart.cs
+++ Assets/Scripts/Player/PlayerControllerKart.cs
@@ -9,13 +9,14 @@ public class PlayerControllerKart : PlayerController {
     float forwardSpeed = 5000.0f;
     [SerializeField]
     float turnSpeed = 6000.0f;
-
+    GamePadState previousState;
     Vector3 targetForward;
 
     void Start () {
         player = GetComponent<Player>();
         Rb = GetComponent<Rigidbody>();
         targetForward = transform.forward;
+        previousState = GamePad.GetState(PlayerIndex);
     }
 
 	void Update () {
@@ -24,12 +25,18 @@ public class PlayerControllerKart : PlayerController {
 
         rb.AddForce(-transform.forward * Time.deltaTime * forwardSpeed/2.0f * state.Triggers.Left);
 
+        if(state.Buttons.X == ButtonState.Pressed && previousState.Buttons.X == ButtonState.Released)
+        {
+            rb.AddForce(transform.forward * 40.0f, ForceMode.Impulse);
+        }
+
         targetForward = new Vector3(state.ThumbSticks.Left.X, 0, state.ThumbSticks.Left.Y);
         if (targetForward == Vector3.zero)
             targetForward = transform.forward;
         transform.rotation = Quaternion.LookRotation(Vector3.Lerp(transform.forward, targetForward, Time.deltaTime * 6.0f * Mathf.Clamp(rb.velocity.magnitude/2.0f, 0.0f, 1.0f)), Vector3.up);
         //transform.rotation = Quaternion.LookRotation(Vector3.Lerp(transform.forward, rb.velocity.normalized, Time.deltaTime * 10.0f));
         rb.AddForce(Vector3.down * Time.deltaTime * 4000.0f);
+        previousState = state;
 	}
 
     private void OnCollisionEnter(Collision collision)
@@ -37,7 +44,7 @@ public class PlayerControllerKart : PlayerController {
         if(collision.gameObject.GetComponent<PlayerControllerKart>())
         {
             collision.gameObject.GetComponent<Rigidbody>().AddForce(-collision.contacts[0].normal * 10.0f, ForceMode.Impulse);
-            Rb.AddForce(collision.contacts[0].normal * 10.0f, ForceMode.Impulse);
+            Rb.AddForce(collision.contacts[0].normal * collision.impulse.magnitude, ForceMode.Impulse);
         }
     }
 }
