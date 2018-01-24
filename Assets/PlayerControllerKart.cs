c83b706ea6720cbb289ff3fc5a5fea41147c5553
Unknown <Sebdami@hotmail.fr>
2018-01-24 17:08:32 +0100

    Start Adding kart minigame

---
+++ Assets/PlayerControllerKart.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UWPAndXInput;
+
+public class PlayerControllerKart : PlayerController {
+
+    [SerializeField]
+    float forwardSpeed = 5000.0f;
+    [SerializeField]
+    float turnSpeed = 6000.0f;
+
+    Vector3 targetForward;
+
+    void Start () {
+        player = GetComponent<Player>();
+        Rb = GetComponent<Rigidbody>();
+        targetForward = transform.forward;
+    }
+
+	void Update () {
+        GamePadState state = GamePad.GetState(PlayerIndex);
+        rb.AddForce(transform.forward * Time.deltaTime * forwardSpeed * state.Triggers.Right);
+
+        rb.AddForce(-transform.forward * Time.deltaTime * forwardSpeed/2.0f * state.Triggers.Left);
+
+        targetForward = new Vector3(state.ThumbSticks.Left.X, 0, state.ThumbSticks.Left.Y);
+        if (targetForward == Vector3.zero)
+            targetForward = transform.forward;
+        transform.rotation = Quaternion.LookRotation(Vector3.Lerp(transform.forward, targetForward, Time.deltaTime * 6.0f * Mathf.Clamp(rb.velocity.magnitude/2.0f, 0.0f, 1.0f)), Vector3.up);
+        //transform.rotation = Quaternion.LookRotation(Vector3.Lerp(transform.forward, rb.velocity.normalized, Time.deltaTime * 10.0f));
+        rb.AddForce(Vector3.down * Time.deltaTime * 4000.0f);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerControllerKart>())
+        {
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(-collision.contacts[0].normal * 10.0f, ForceMode.Impulse);
+            Rb.AddForce(collision.contacts[0].normal * 10.0f, ForceMode.Impulse);
+        }
+    }
+}
---
+++ Assets/Scripts/GameMode/KartGameMode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UWPAndXInput;
+public class KartGameMode : GameMode {
+
+    public float timer;
+
+    public override void StartGame(List<GameObject> playerReferences)
+    {
+        base.StartGame(playerReferences);
+
+        LaunchTimer();
+    }
+
+    public void LaunchTimer()
+    {
+        GameManager.Instance.GameFinalTimer = timer;
+        GameManager.Instance.LaunchFinalTimer();
+    }
+
+    public override void AttributeCamera(uint activePlayersAtStart, GameObject[] cameraReferences, List<GameObject> playersReference)
+    {
+    }
+
+    public override void PlayerHasFinished(Player player)
+    {
+        GameManager.Instance.ScoreScreenReference.RankPlayersByPoints();
+    }
+}
