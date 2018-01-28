7221b31c29dd48315118c345e775354831947674
Unknown <olivier.serris@wanadoo.fr>
2018-01-28 22:07:43 +0100

    Added Easing proto to level generation

--- Assets/Scripts/MiniGames/Runner3D/RunnerBlocs.cs
+++ Assets/Scripts/MiniGames/Runner3D/RunnerBlocs.cs
@@ -2,12 +2,97 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RunnerBlocs : MonoBehaviour {
+namespace Runner3D
+{
 
-    [SerializeField] Vector3 size;
-    [SerializeField] Vector3 blockSize;
-    public void OnValidate()
+    public enum Direction
     {
-        
+        Up,
+        Down,
+    }
+
+    public class RunnerBlocs : MonoBehaviour {
+
+        float[] baseYPos;
+        Vector3[] baseScale;
+
+        static float yInterval = 10;
+        [SerializeField] Vector3 blockSize;
+        public void Awake()
+        {
+            baseYPos = new float[transform.childCount];
+            baseScale = new Vector3[transform.childCount];
+            for (int i = 0; i < transform.childCount;i++)
+            {
+                    // register base position
+                baseYPos[i] = transform.position.y;
+                baseScale[i] = transform.localScale;
+                // goDown at beginning : 
+            }
+        }
+        public void Start()
+        {
+            Invoke("TpDown", 0.1f);
+        }
+        public void TpDown()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Transform child = transform.GetChild(i);
+                child.position = new Vector3(child.position.x, child.position.y - yInterval, child.position.z);
+                child.localScale = Vector3.zero;
+            }
+        }
+
+        public void OnValidate()
+        {
+            blockSize.x = Mathf.Round(blockSize.x);
+            blockSize.y = Mathf.Round(blockSize.y);
+            blockSize.z = Mathf.Round(blockSize.z);
+        }
+        public void LauchLerp(Direction dir)
+        {
+            StartCoroutine(LerpDown(dir));
+        }
+        IEnumerator LerpDown(Direction dir)
+        {
+            float timer = 0;
+            while (timer < 1) 
+            {
+                timer += Time.deltaTime*0.85f;
+                float factor = Ease.Evaluate(Ease.EASE_TYPE.BOUNCE_OUT, timer);
+                for (int i = 0; i < transform.childCount;i++)
+                {
+                    Transform child = transform.GetChild(i);
+                    if (child.GetComponent<PlatformGameplay>())
+                    {
+                        child.GetComponent<PlatformGameplay>().IsMoving = false;
+                    }
+                    Vector3 nextPosition = child.position;
+                    if (dir == Direction.Up)
+                    {
+                        nextPosition.y = Mathf.Lerp(baseYPos[i] - yInterval, baseYPos[i], factor);
+                        child.localScale = Vector3.Lerp(Vector3.zero, baseScale[i], factor);
+                    }
+                    else
+                    {
+                        nextPosition.y = Mathf.Lerp(baseYPos[i], baseYPos[i] - yInterval, factor);
+                        child.localScale = Vector3.Lerp(baseScale[i], Vector3.zero, factor);
+                    }
+                    child.position = nextPosition;
+                }
+                yield return null;
+            }
+            if (dir == Direction.Up)
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    Transform child = transform.GetChild(i);
+                    if (child.GetComponent<PlatformGameplay>())
+                        child.GetComponent<PlatformGameplay>().IsMoving = true;
+                }
+            yield return null;
+        }
+
+
     }
 }
--- Assets/Scripts/MiniGames/Runner3D/RunnerLevelGenerator.cs
+++ Assets/Scripts/MiniGames/Runner3D/RunnerLevelGenerator.cs
@@ -2,80 +2,147 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RunnerLevelGenerator : MonoBehaviour {
+namespace Runner3D
+{
+    public class RunnerLevelGenerator : MonoBehaviour
+    {
 
         //  ref vers la pool. 
         //TODO: A transformer en tableau pour contrôler la fréquence des blocs
-    Pool runnerBlocPool;
-        
-    public static readonly Vector3 defaultBlockSize = Vector3.one * 20; // Taille du plus petit bloc possible
-    [SerializeField]Vector3 levelUnit; // taille d'un niveau en nb de blocs
-    [SerializeField]Vector3 leveFinalSize;  // taille réelle du niveau
-    public void OnValidate()
-    {
-        levelUnit.x = Mathf.Round(levelUnit.x);
-        levelUnit.y = Mathf.Round(levelUnit.y);
-        levelUnit.z = Mathf.Round(levelUnit.z);
+        Pool runnerBlocPool;
+        Player[] playerRef;
 
-        leveFinalSize.x = levelUnit.x * defaultBlockSize.x;
-        leveFinalSize.y = levelUnit.y * defaultBlockSize.y;
-        leveFinalSize.z = levelUnit.z * defaultBlockSize.z;
-    }
-  
+        RunnerBlocs[,] blockMap;
+        public static readonly Vector3 defaultBlockSize = Vector3.one * 20; // Taille du plus petit bloc possible
+        [SerializeField] Vector3 levelUnit; // taille d'un niveau en nb de blocs
+        [SerializeField] Vector3 leveFinalSize;  // taille réelle du niveau
+        public void OnValidate()
+        {
+            levelUnit.x = Mathf.Round(levelUnit.x);
+            levelUnit.y = Mathf.Round(levelUnit.y);
+            levelUnit.z = Mathf.Round(levelUnit.z);
+
+            leveFinalSize.x = levelUnit.x * defaultBlockSize.x;
+            leveFinalSize.y = levelUnit.y * defaultBlockSize.y;
+            leveFinalSize.z = levelUnit.z * defaultBlockSize.z;
+        }
+
         // Fill the level according to the mask.
-    public void GenerateLevelBlock(bool[,] mask)
-    {
-        for (int x = 0; x < levelUnit.x; x++)
+        public void GenerateLevelBlock(bool[,] mask)
         {
-            for (int z = 0; z < levelUnit.z; z++)
+            blockMap = new RunnerBlocs[(int)levelUnit.x, (int)levelUnit.z];
+            for (int x = 0; x < levelUnit.x; x++)
             {
-                if (mask[z, x])
+                for (int z = 0; z < levelUnit.z; z++)
                 {
-                    Vector3 position = new Vector3(x * defaultBlockSize.x, 0, z * defaultBlockSize.z);
-                    runnerBlocPool.GetItem(transform, position, Quaternion.identity).SetActive(true);
+                    if (mask[z, x])
+                    {
+                        Vector3 position = new Vector3(x * defaultBlockSize.x, 0, z * defaultBlockSize.z);
+                        GameObject bloc = runnerBlocPool.GetItem(transform, position, Quaternion.identity);
+                        bloc.SetActive(true);
+                        blockMap[x, z] = bloc.GetComponent<RunnerBlocs>();
+                    }
                 }
-
             }
         }
-    }
-    public bool[,] GenerateLevelMask()
-    {
-        bool[,] mask = new bool[(int)levelUnit.z, (int)levelUnit.x];
-
-        // random walker algorithme to fill the level mask
-        int posInLine = Random.Range(0, (int)levelUnit.x);
-        mask[0, posInLine] = true;
-
-        for (int z = 1; z < levelUnit.z; z++)
+        public bool[,] GenerateLevelMask()
         {
-            posInLine += Random.Range(-1, 2);
-            posInLine = posInLine < 0 ? posInLine + 1 : posInLine;
-            posInLine = posInLine >= levelUnit.x ? (int)levelUnit.x - 1 : posInLine;
-            mask[z, posInLine] = true;
-        }
-        return mask;
-    }
+            bool[,] mask = new bool[(int)levelUnit.z, (int)levelUnit.x];
 
-    public void Generate2D()
-    {
-        bool[,] mask = GenerateLevelMask();
-        GenerateLevelBlock(mask);
-    }
+            // random walker algorithme to fill the level mask
+            int posInLine = Random.Range(0, (int)levelUnit.x);
+            mask[0, posInLine] = true;
 
-    public void Start()
-    {
-        runnerBlocPool = ResourceUtils.Instance.poolManager.runnerBlocPool;
-        Invoke("Generate2D", 0.4f);
-    }
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            for (int z = 0; z < transform.childCount; z++)
+            for (int z = 1; z < levelUnit.z; z++)
             {
-                Destroy(transform.GetChild(z).gameObject);
+                posInLine += Random.Range(-1, 2);
+                posInLine = posInLine < 0 ? posInLine + 1 : posInLine;
+                posInLine = posInLine >= levelUnit.x ? (int)levelUnit.x - 1 : posInLine;
+                mask[z, posInLine] = true;
             }
-            Generate2D();
+            return mask;
+        }
+        public void Generate2D()
+        {
+            bool[,] mask = GenerateLevelMask();
+            GenerateLevelBlock(mask);
+            StartCoroutine(LevelPresentation());
+        }
+
+        int firstPlayerZPos = 0;
+        int nbRowUpBehindFirst = 2;
+        int nbRowUpInFrontFirst = 2;
+        public void MoveCursor(int newCursorValue)
+        {
+            int variation = newCursorValue - firstPlayerZPos;
+            if (variation > 0)
+            {
+                for (int i = firstPlayerZPos + 1; i <= firstPlayerZPos + variation; i++)
+                {
+                    LerpMessage(i + nbRowUpInFrontFirst, Direction.Up);
+                    LerpMessage(i - nbRowUpBehindFirst, Direction.Down);
+                }
+            }
+            else if (variation < 0) // Pas encore testé.
+                for (int i = firstPlayerZPos; i >= firstPlayerZPos - variation; i--)
+                {
+                    LerpMessage(firstPlayerZPos + i, Direction.Down);
+                }
+            else
+                Debug.LogWarning("This function shouldn't be called");
+            firstPlayerZPos = newCursorValue;
+        }
+        public void LerpMessage(int row,Direction dir)
+        {
+            if (row < 0 || row >= levelUnit.z)
+                return;
+            Debug.Log("Line : " + row + "go " + dir);
+            for (int x = 0; x < levelUnit.x; x++)
+            {
+                if (blockMap[x, row] != null)
+                    blockMap[x, row].LauchLerp(dir);
+            }
+        }
+
+        public void UpdatePlayerPos()
+        {
+            Vector3[] playerNewPos = new Vector3[playerRef.Length];
+            float farthestZ = 0;
+            for (int i = 0; i < playerRef.Length; i++)
+                farthestZ = Mathf.Max(playerRef[i].transform.position.z, farthestZ);
+            if (farthestZ != firstPlayerZPos)
+            {
+                float test = farthestZ / defaultBlockSize.z;
+                int playerZBlockPos = Mathf.RoundToInt(farthestZ / defaultBlockSize.z);
+            }
+        }
+
+        public void Start()
+        {
+            runnerBlocPool = ResourceUtils.Instance.poolManager.runnerBlocPool;
+            Invoke("Generate2D", 0.4f);
+        }
+        public void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                for (int z = 0; z < transform.childCount; z++)
+                {
+                    Destroy(transform.GetChild(z).gameObject);
+                }
+                Generate2D();
+            }
+        }
+        public IEnumerator LevelPresentation()
+        {
+            yield return new WaitForSeconds(0.5f);
+            firstPlayerZPos = -nbRowUpInFrontFirst - 1;
+            for (int i = - nbRowUpInFrontFirst; i < levelUnit.z+ nbRowUpBehindFirst; i++)
+            {
+                MoveCursor(i);
+                yield return new WaitForSeconds(0.5f);
+            }
+            yield return null;
         }
     }
 }
