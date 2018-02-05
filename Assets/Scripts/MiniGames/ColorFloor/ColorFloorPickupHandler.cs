c44a63b68ae161e5123c5244771c149a69740bde
Unknown <transkuja@hotmail.fr>
2018-02-05 16:09:20 +0100

    Fix PickupHandler (CF)

--- Assets/Scripts/MiniGames/ColorFloor/ColorFloorPickupHandler.cs
+++ Assets/Scripts/MiniGames/ColorFloor/ColorFloorPickupHandler.cs
@@ -16,12 +16,13 @@ public class ColorFloorPickupHandler : MonoBehaviour {
     int lineCount;
     int lineSize;
 
-    IEnumerator Start () {
+    IEnumerator Start()
+    {
         lineCount = transform.childCount;
         for (int i = 0; i < transform.childCount; i++)
             mapSize += transform.GetChild(i).childCount;
-        
-		while (true)
+
+        while (true)
         {
             yield return new WaitForSeconds(pickupDelay);
             if (pickupSpawned < maxPickupOnMap)
@@ -31,26 +32,16 @@ public class ColorFloorPickupHandler : MonoBehaviour {
                 lineSize = transform.GetChild(randChild / lineCount).childCount;
 
                 // Makes sure we don't spawn twice at the same place
-                while (transform.GetChild(randChild/lineCount).GetChild(randChild%lineSize).childCount > 0)
+                while (transform.GetChild(randChild / lineCount).GetChild(randChild % lineSize).childCount > 0)
                     randChild = Random.Range(0, mapSize);
 
                 ResourceUtils.Instance.poolManager.GetPoolByName(PoolName.ColorFloorScorePickUp).GetItem(
                     transform.GetChild(randChild / lineCount).GetChild(randChild % lineSize),
-                    Vector3.up, 
+                    Vector3.up,
                     Quaternion.identity,
                     true
                 );
-
-  //              ResourceUtils.Instance.poolManager.GetPoolByName(PoolName.ColorFloorScorePickUp).GetItem(
-  //                  transform.GetChild(randChild / lineCount).GetChild(randChild % lineSize),
-  //                  Vector3.up, 
-  //                  Quaternion.identity,
-  //                  true
-  //              );
-
-  //              pickupSpawned++;
-  //          }
-  //      }
-	//}
-
+            }
+        }
+    }
 }
