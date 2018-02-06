ec26d39a81445132d34c32dde49f2092b36610fb
Unknown <transkuja@hotmail.fr>
2018-02-06 21:19:03 +0100

    Fix buffs stack on Evolution Strength too

--- Assets/Scripts/Evolutions/EvolutionStrength.cs
+++ Assets/Scripts/Evolutions/EvolutionStrength.cs
@@ -6,16 +6,24 @@ using UWPAndXInput;
 
 public class EvolutionStrength : EvolutionComponent
 {
+    StatBuff groundSpeedBuff = new StatBuff(Stats.StatType.GROUND_SPEED, 0.85f, -1);
+
     public override void  Start()
     {
         base.Start();
         SetPower(Powers.Strength);
-        playerController.stats.AddBuff(new StatBuff(Stats.StatType.GROUND_SPEED, 0.85f, -1));
+        playerController.stats.AddBuff(groundSpeedBuff);
         Player playerComponent = GetComponent<Player>();
         if (!playerComponent.evolutionTutoShown[(int)Powers.Strength] && !GameManager.Instance.CurrentGameMode.IsMiniGame())
         {
             playerComponent.evolutionTutoShown[(int)Powers.Strength] = true;
             Utils.PopTutoText("Press Y in the air", playerComponent);
         }
+    }
+
+    protected override void OnDestroy()
+    {
+        playerController.stats.RemoveBuff(groundSpeedBuff);
+        base.OnDestroy();
     }
 }
