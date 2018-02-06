d138b15da662daf1791c9f63bb950dd915de3a9b
Unknown <transkuja@hotmail.fr>
2018-02-06 22:43:36 +0100

    Create Rule screen states handler

---
+++ Assets/Scripts/UI/RuleScreenHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuleScreenHandler : MonoBehaviour {
+    enum RuleScreenState { FirstPage, ControlsPage, PickupPage }
+    RuleScreenState curState = RuleScreenState.FirstPage;
+
+    private RuleScreenState CurState {
+        set
+        {
+            for (int i = 1; i < transform.childCount; i++)
+            {
+                if (i - 1 == (int)value)
+                {
+                    transform.GetChild(i).gameObject.SetActive(true);
+                }
+                else
+                    transform.GetChild(i).gameObject.SetActive(false);
+            }
+            
+            curState = value;
+        }
+    }
+
+    void Start () {
+        CurState = RuleScreenState.FirstPage;
+    }
+
+    void ChangeState(bool _stateForward)
+    {
+        if (_stateForward && curState != RuleScreenState.PickupPage)
+            CurState = (RuleScreenState)(int)curState + 1;
+
+        if (!_stateForward && curState != RuleScreenState.FirstPage)
+            CurState = (RuleScreenState)(int)curState - 1;
+    }
+}
