fdc66a2776f1a5032e8e321504be9b93f9a2a2e4
Unknown <transkuja@hotmail.fr>
2018-02-07 00:18:17 +0100

    ColorFloor: extract specific code

---
+++ Assets/Scripts/MiniGames/ColorFloor/ColorFloorPickUp.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorFloorPickUp : MinigamePickUp {
+
+    [SerializeField]
+    float speedBoostDuration = 10.0f;
+
+    [SerializeField]
+    float colorArrowRotationDelay = 2.0f;
+
+    IEnumerator Start()
+    {
+        InitializeDelegates(pickupType);
+        if (pickupType == PickUpType.ColorArrow)
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(colorArrowRotationDelay);
+                transform.localEulerAngles += Vector3.up * 90;
+            }
+        }
+    }
+
+    void ScorePoints(int _playerIndex)
+    {
+        ColorFloorHandler.ScorePoints(_playerIndex);
+    }
+
+    void ColorFloorWithPickup(int _playerIndex)
+    {
+        ColorFloorHandler.ColorFloorWithPickup(this, _playerIndex);
+    }
+
+    void UseBomb(int _playerIndex)
+    {
+
+    }
+
+    void UseMissile(int _playerIndex)
+    {
+
+    }
+
+    void UseSpeedUp(int _playerIndex)
+    {
+        GameManager.EvolutionManager.AddEvolutionComponent(
+            GameManager.Instance.PlayerStart.PlayersReference[_playerIndex],
+            GameManager.EvolutionManager.GetEvolutionByPowerName(Powers.Agile),
+            true,
+            speedBoostDuration
+        );
+    }
+
+    protected override void InitializeDelegates(PickUpType _pickupType)
+    {
+        switch (_pickupType)
+        {
+            case PickUpType.Score:
+            case PickUpType.ColorArrow:
+            case PickUpType.ColorAround:
+            case PickUpType.SpeedUp:
+                collectPickup = InstantUse;
+                break;
+
+            default:
+                collectPickup = StoreAndUseLater;
+                break;
+        }
+
+        switch (_pickupType)
+        {
+            case PickUpType.Score:
+                usePickup = ScorePoints;
+                break;
+            case PickUpType.ColorArrow:
+            case PickUpType.ColorAround:
+                usePickup = ColorFloorWithPickup;
+                break;
+            case PickUpType.SpeedUp:
+                usePickup = UseSpeedUp;
+                break;
+            case PickUpType.Missile:
+                usePickup = UseMissile;
+                break;
+            case PickUpType.Bomb:
+                usePickup = UseBomb;
+                break;
+        }
+
+        if (usePickup == null)
+            Debug.LogWarning("No use method defined for pickup " + _pickupType);
+    }
+}
---
+++ Assets/Scripts/MiniGames/MinigamePickUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PickUpType { Score, ColorArrow, ColorAround, SpeedUp, Bomb, Missile }
+/*
+ * Each minigame using pickups should have a MyMinigamePickUp inherited from this class with InitializeDelegates overriden.
+ */
+public class MinigamePickUp : MonoBehaviour {
+
+    public PickUpType pickupType;
+    public delegate void Collect(int _playerIndex);
+    public delegate void Use(int _playerIndex);
+
+    public Collect collectPickup;
+    public Use usePickup;
+
+    public void StoreAndUseLater(int _playerIndex)
+    {
+        GameManager.Instance.PlayerStart.PlayersReference[_playerIndex].GetComponent<Player>().currentStoredPickup = this;
+    }
+
+    public void InstantUse(int _playerIndex)
+    {
+        usePickup(_playerIndex);
+    }
+
+    // Should be overriden in child class then called in child class' Start method
+    protected virtual void InitializeDelegates(PickUpType _pickupType) { }
+}
