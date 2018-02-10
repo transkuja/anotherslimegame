57e1657359184ad94df99325ff66e54dc08916fa
Unknown <transkuja@hotmail.fr>
2018-02-10 20:13:49 +0100

    ColorFloor: Add terrain and life (like God)

---
+++ Assets/Scripts/Player/PNJRandomExpression.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PNJRandomExpression : MonoBehaviour {
+
+    PlayerCosmetics cosmeticsComp;
+    [SerializeField]
+    bool hidingPNJSpecialBehaviourInColorFloorSceneButIWasTooLazyToDoAnotherScript = false;
+
+
+    Vector3 lerpOrigin;
+    Vector3 lerpDest;
+    float lerpParam = 0.0f;
+    bool isLerping = false;
+
+    IEnumerator Start()
+    {
+        cosmeticsComp = GetComponentInChildren<PlayerCosmetics>();
+        if (hidingPNJSpecialBehaviourInColorFloorSceneButIWasTooLazyToDoAnotherScript)
+            StartCoroutine(Hide());
+
+        while (true)
+        {
+            yield return new WaitForSeconds(6.0f);
+            cosmeticsComp.FaceEmotion = (FaceEmotion)Random.Range(1, 4);
+            yield return new WaitForSeconds(2.0f);
+            cosmeticsComp.FaceEmotion = FaceEmotion.Neutral;
+        }
+    }
+
+    IEnumerator Hide()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(8.0f);
+            InitLerp(Vector3.up);
+            yield return new WaitForSeconds(4.0f);
+            InitLerp(Vector3.down);
+
+        }
+    }
+
+    void InitLerp(Vector3 _lerpDir)
+    {
+        lerpOrigin = transform.position;
+        lerpParam = 0.0f;
+        lerpDest = lerpOrigin + _lerpDir;
+        isLerping = true;
+    }
+
+    void Update()
+    {
+       if (isLerping)
+       {
+            lerpParam += Time.deltaTime;
+            transform.position = Vector3.Lerp(lerpOrigin, lerpDest, Mathf.Clamp(lerpParam, 0, 1.0f));
+            if (lerpParam > 1.0f)
+                isLerping = false;
+       }
+    }
+}
