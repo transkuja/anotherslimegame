d5bb5070ddd2338df3afe54eb6d53da433898075
Unknown <Sebdami@hotmail.fr>
2018-02-13 23:31:57 +0100

    Fix another stupid mistake

--- Assets/Scripts/ForbiddenSection/ReplaceWithPrefab.cs
+++ Assets/Scripts/ForbiddenSection/ReplaceWithPrefab.cs
@@ -37,7 +37,7 @@ public class ReplaceWithPrefab : MonoBehaviour {
     GameObject parapetMossy;
 
     [SerializeField]
-    bool replace = true;
+    bool replace = false;
 	// Use this for initialization
 	void Start () {
 		
