2df3ade9f64cddbbd30c323cd41eeb2133ff5b8b
Unknown <Sebdami@hotmail.fr>
2018-02-14 17:41:16 +0100

    Add Boost Tile

---
+++ Assets/Scripts/BoostTile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostTile : MonoBehaviour {
+
+    [SerializeField]
+    float boostForce = 1000.0f;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.GetComponentInParent<PlayerControllerKart>())
+        {
+            other.GetComponentInParent<Rigidbody>().AddForce(transform.forward * boostForce);
+        }
+    }
+}
---
+++ Assets/Scripts/BoostTileMaterialHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoostTileMaterialHandler : MonoBehaviour {
+    [SerializeField]
+    float scrollSpeed = 10.0f;
+    Material mat;
+	// Use this for initialization
+	void Start () {
+        mat = GetComponent<MeshRenderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mat.SetTextureOffset("_DetailAlbedoMap", mat.GetTextureOffset("_DetailAlbedoMap") + Vector2.up * Time.deltaTime * scrollSpeed);
+	}
+}
