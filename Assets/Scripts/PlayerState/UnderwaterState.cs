8b1a998647016e75e2e9f944154821382c807810
Unknown <remi.boscher@gmail.com>
2018-03-13 16:59:59 +0100

    Water animation

--- Assets/Scripts/PlayerState/UnderwaterState.cs
+++ Assets/Scripts/PlayerState/UnderwaterState.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class UnderwaterState : PlayerState
 {
     public bool hasReachedTheSurface = false;
+    bool hasStartedGoingUp = false;
+
     public float waterLevel;
     float waterTolerance;
     public UnderwaterState(PlayerControllerHub _playerController) : base(_playerController)
@@ -25,7 +27,9 @@ public class UnderwaterState : PlayerState
     public override void OnEnd()
     {
         base.OnEnd();
+        playerController.Player.Anim.SetBool("isBoobbing", false);
         hasReachedTheSurface = false;
+        hasStartedGoingUp = false;
     }
 
     public override void OnFixedUpdate()
@@ -42,8 +46,12 @@ public class UnderwaterState : PlayerState
     public override void OnUpdate()
     {
         base.OnUpdate();
-        if (playerController.transform.position.y > waterLevel - waterTolerance)
+        if (!hasReachedTheSurface && hasStartedGoingUp && playerController.transform.position.y > waterLevel - waterTolerance)
+        {
             hasReachedTheSurface = true;
+            playerController.Player.Anim.SetBool("isBoobbing", true);
+        }
+
     }
 
     public override void OnJumpPressed()
@@ -59,9 +67,22 @@ public class UnderwaterState : PlayerState
     // Disable gravity when underwater
     public override void HandleGravity()
     {
-        if (!hasReachedTheSurface)
+        if (playerController.transform.position.y < waterLevel - waterTolerance - 1f)
+        {
+            hasStartedGoingUp = true;
+        }
+
+        if (!hasReachedTheSurface && hasStartedGoingUp)
+        {
             playerController.Player.Rb.AddForce(30 * Vector3.up);
-        else
+        }
+
+        if (!hasStartedGoingUp && playerController.transform.position.y < waterLevel - waterTolerance && playerController.transform.position.y > waterLevel - waterTolerance - 1f)
+        {
+            playerController.Player.Rb.AddForce(90 * Vector3.down);
+        }
+
+        if (hasReachedTheSurface)
             playerController.Player.Rb.velocity = new Vector3(playerController.Player.Rb.velocity.x, 0, playerController.Player.Rb.velocity.z);
     }
 }
