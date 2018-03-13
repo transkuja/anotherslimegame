59f9a5d07aab0860436899d663a556f3155d10ef
Unknown <transkuja@hotmail.fr>
2018-03-13 08:58:46 +0100

    Update UI minigame + timer

--- Assets/Scripts/UI/APlayerUI.cs
+++ Assets/Scripts/UI/APlayerUI.cs
@@ -1,11 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
+/// <summary>
+/// Handle players' ui in minigames. The desired behaviour is as follow, with a X for "ui shown" and a - for "ui not shown":
+/// 1P: - X 00:00 - -
+/// 2P: - X 00:00 X -
+/// 3P: X X 00:00 X -
+/// 4P: X X 00:00 X -
+/// /// </summary>
 public abstract class APlayerUI : MonoBehaviour {
 
-    public GameObject UIref;
+    [Tooltip("The 2 players' ui shown in the top left corner.")]
+    public GameObject UIrefLeft;
+    [Tooltip("The 2 players' ui shown in the top right corner.")]
+    public GameObject UIrefRight;
 
     public void Awake()
     {
@@ -14,13 +23,7 @@ public abstract class APlayerUI : MonoBehaviour {
 
     public virtual void Init()
     {
-        for (int i = 0; i < UIref.transform.childCount; i++)
-        {
-            if (GameManager.Instance.PlayerStart.ActivePlayersAtStart > i)
-                UIref.transform.GetChild(i).gameObject.SetActive(true);
-            else
-                UIref.transform.GetChild(i).gameObject.SetActive(false);
-        }
+        ShowXPlayersUI(GameManager.Instance.PlayerStart.ActivePlayersAtStart);
         foreach (GameObject p in GameManager.Instance.PlayerStart.PlayersReference)
             p.GetComponent<Player>().OnValuesChange = new UIfct[(int)PlayerUIStat.Size];
 
@@ -35,5 +38,40 @@ public abstract class APlayerUI : MonoBehaviour {
     public virtual void OnValueChange(TextChange text)
     {
         // feedback 
+    }
+
+    void ShowXPlayersUI(uint _nbrPlayers)
+    {
+        switch (_nbrPlayers)
+        {
+            // 1P: - X 00:00 - -
+            case 1:
+                UIrefLeft.transform.GetChild(0).gameObject.SetActive(false);
+                UIrefLeft.transform.GetChild(1).gameObject.SetActive(true);
+                UIrefRight.transform.GetChild(0).gameObject.SetActive(false);
+                UIrefRight.transform.GetChild(1).gameObject.SetActive(false);
+                break;
+            // 2P: - X 00:00 X -
+            case 2:
+                UIrefLeft.transform.GetChild(0).gameObject.SetActive(false);
+                UIrefLeft.transform.GetChild(1).gameObject.SetActive(true);
+                UIrefRight.transform.GetChild(0).gameObject.SetActive(true);
+                UIrefRight.transform.GetChild(1).gameObject.SetActive(false);
+                break;
+            // 3P: X X 00:00 X -
+            case 3:
+                UIrefLeft.transform.GetChild(0).gameObject.SetActive(true);
+                UIrefLeft.transform.GetChild(1).gameObject.SetActive(true);
+                UIrefRight.transform.GetChild(0).gameObject.SetActive(true);
+                UIrefRight.transform.GetChild(1).gameObject.SetActive(false);
+                break;
+            // 4P: X X 00:00 X -
+            default:
+                UIrefLeft.transform.GetChild(0).gameObject.SetActive(true);
+                UIrefLeft.transform.GetChild(1).gameObject.SetActive(true);
+                UIrefRight.transform.GetChild(0).gameObject.SetActive(true);
+                UIrefRight.transform.GetChild(1).gameObject.SetActive(true);
+                break;
+        }
     }
 }
--- Assets/Scripts/UI/PlayerUIPushGame.cs
+++ Assets/Scripts/UI/PlayerUIPushGame.cs
@@ -9,13 +9,41 @@ public class PlayerUIPushGame : APlayerUI {
         base.Init();        
         for (int i = 0; i < GameManager.Instance.PlayerStart.PlayersReference.Count; i++)
         {
-            if (SlimeDataContainer.instance != null)
-                //UIref.transform.GetChild(i).GetChild(0).GetComponent<Image>().color = (SlimeDataContainer.instance.colorFadeSelected[i] ? SlimeDataContainer.instance.selectedColors[i] : Color.white);
-                // TMP
-                UIref.transform.GetChild(i).GetChild(0).GetComponent<Image>().color = Color.white;
-
             GameManager.Instance.PlayerStart.PlayersReference[i].GetComponent<Player>().CallOnValueChange(PlayerUIStat.Points, 0);
         }
 
+        UpdateSpriteColorBasedOnSelection();
+       
+    }
+
+    private void UpdateSpriteColorBasedOnSelection()
+    {
+        if (SlimeDataContainer.instance != null)
+        {
+            //UIref.transform.GetChild(i).GetChild(0).GetComponent<Image>().color = (SlimeDataContainer.instance.colorFadeSelected[i] ? SlimeDataContainer.instance.selectedColors[i] : Color.white);
+            
+            // TMP: Should use color from slimedatacontainer instead
+            switch (GameManager.Instance.PlayerStart.ActivePlayersAtStart)
+            {
+                case 1:
+                    UIrefLeft.transform.GetChild(1).GetChild(0).GetComponent<Image>().color = Color.white;
+                    break;
+                case 2:
+                    UIrefLeft.transform.GetChild(1).GetChild(0).GetComponent<Image>().color = Color.white;
+                    UIrefRight.transform.GetChild(0).GetChild(0).GetComponent<Image>().color = Color.white;
+                    break;
+                case 3:
+                    UIrefLeft.transform.GetChild(0).GetChild(0).GetComponent<Image>().color = Color.white;
+                    UIrefLeft.transform.GetChild(1).GetChild(0).GetComponent<Image>().color = Color.white;
+                    UIrefRight.transform.GetChild(0).GetChild(0).GetComponent<Image>().color = Color.white;
+                    break;
+                default:
+                    UIrefLeft.transform.GetChild(0).GetChild(0).GetComponent<Image>().color = Color.white;
+                    UIrefLeft.transform.GetChild(1).GetChild(0).GetComponent<Image>().color = Color.white;
+                    UIrefRight.transform.GetChild(0).GetChild(0).GetComponent<Image>().color = Color.white;
+                    UIrefRight.transform.GetChild(1).GetChild(0).GetComponent<Image>().color = Color.white;
+                    break;
+            }
+        }
     }
 }
