d897f36b971272d0657348e11a0b823b7ac9b0b5
Unknown <Sebdami@hotmail.fr>
2018-03-15 12:28:29 +0100

    Kart: LD Quick update, use points to count laps

--- Assets/Scripts/KartArrival.cs
+++ Assets/Scripts/KartArrival.cs
@@ -9,11 +9,9 @@ public class KartArrival : MonoBehaviour {
 
     [SerializeField]
     CheckPoint lastCheckpoint;
-    int[] playerLaps;
 
     private void Start()
     {
-        playerLaps = new int[4];
         ((KartGameMode)GameManager.Instance.CurrentGameMode).Arrival = this;
     }
 
@@ -23,11 +21,11 @@ public class KartArrival : MonoBehaviour {
         float angle = Vector3.Angle(transform.forward, other.gameObject.GetComponent<Rigidbody>().velocity);
         if (player && angle < 90.0f )
         {
-            if(player.respawnPoint == lastCheckpoint.transform)
+            if(player.respawnPoint == lastCheckpoint.transform && player.NbPoints < 3)
             {
-                playerLaps[player.ID]++;
-                player.CallOnValueChange(PlayerUIStat.Laps, playerLaps[player.ID]);
-                if (playerLaps[player.ID] >= NumberOfLaps)
+                player.NbPoints++;
+                //player.CallOnValueChange(PlayerUIStat.Points, player.NbPoints);
+                if (player.NbPoints >= NumberOfLaps)
                 {
                     //The current player finished the race, disable his controls and save the scores
                     player.GetComponent<PlayerControllerKart>().CurrentState = PlayerControllerKart.KartPlayerState.FinishedRace;
