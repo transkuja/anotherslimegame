91e5342057e95d391bb73b471e2089b28b3a7592
Unknown <olivier.serris@wanadoo.fr>
2018-03-27 11:05:15 +0200

    Debug (Bad) Third Player black Screen

---
+++ Assets/DebugDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugDestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("RemoveBlackScreen",1);
+	}
+    void RemoveBlackScreen()
+    {
+        if (GameManager.Instance.PlayerStart.PlayersReference.Count != 3)
+            Destroy(this.gameObject);
+    }
+
+}
--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -47,8 +47,7 @@ public class UI : MonoBehaviour {
         UIref = transform.GetChild(0);
         ptsText = UIref.GetChild(0).GetComponentInChildren<Text>().transform;
         runeText = UIref.GetChild(1).GetComponentInChildren<Text>().transform;
-        RuleScreen = transform.GetChild(2);
-
+        RuleScreen = GetComponentInChildren<RuleScreenHandler>().transform;
         // Merde copy de reference ..
         ptsTextOriginalState = ptsText;
         runeTextOriginalState = runeText;
