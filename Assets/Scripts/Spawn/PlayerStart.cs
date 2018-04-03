6b5087df33d0e59c6499b5f019e914096cc488de
Unknown <berthetmathieu@free.fr>
2018-04-03 11:18:15 +0200

    Fix de fix

--- Assets/Scripts/Spawn/PlayerStart.cs
+++ Assets/Scripts/Spawn/PlayerStart.cs
@@ -171,7 +171,7 @@ public class PlayerStart : MonoBehaviour {
                     Transform customizableParent = go.transform.GetComponentInChildren<CustomizableSockets>().transform;
                     GameObject mustache = Instantiate(Resources.Load(GameManager.Instance.DataContainer.mustachesSelected[i]), customizableParent.GetChild((int)CustomizableType.Mustache - 2).transform) as GameObject;
                     // Test la présence d'un hinge joint : exemple 3eme moustache n'en a pas
-                    if (GetComponentInChildren<HingeJoint>())
+                    if (mustache.GetComponentInChildren<HingeJoint>())
                     {
                         mustache.transform.GetChild(0).GetChild(0).GetComponent<HingeJoint>().connectedBody = go.GetComponent<Rigidbody>();
                         mustache.transform.GetChild(0).GetChild(1).GetComponent<HingeJoint>().connectedBody = go.GetComponent<Rigidbody>();
