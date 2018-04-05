14ce051394f96165ea29badfcb5c870b7cd9a450
Unknown <transkuja@hotmail.fr>
2018-04-05 09:50:24 +0200

    No cursor outside minigames

--- Assets/CursorPlayerId.cs
+++ Assets/CursorPlayerId.cs
@@ -8,14 +8,17 @@ public class CursorPlayerId : MonoBehaviour {
 	
     IEnumerator Start()
     {
-        Color tmpColor = GameManager.Instance.PlayerStart.colorPlayer[(int)GetComponentInParent<PlayerController>().playerIndex];
-        Color playerColor = new Color(tmpColor.r, tmpColor.g, tmpColor.b, 1.0f);
-        transform.GetChild(0).GetComponentInChildren<Image>().color = playerColor;
-
-        while (true)
+        if (!GameManager.Instance.IsInHub())
         {
-            yield return new WaitForSeconds(2.5f);
-            transform.GetChild(0).gameObject.SetActive(!transform.GetChild(0).gameObject.activeInHierarchy);
+            Color tmpColor = GameManager.Instance.PlayerStart.colorPlayer[(int)GetComponentInParent<PlayerController>().playerIndex];
+            Color playerColor = new Color(tmpColor.r, tmpColor.g, tmpColor.b, 1.0f);
+            transform.GetChild(0).GetComponentInChildren<Image>().color = playerColor;
+
+            while (true)
+            {
+                yield return new WaitForSeconds(2.5f);
+                transform.GetChild(0).gameObject.SetActive(!transform.GetChild(0).gameObject.activeInHierarchy);
+            }
         }
     }
 
