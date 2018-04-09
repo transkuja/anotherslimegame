0eef9099b5810ff3880bace9a213b088981a5e4e
Unknown <berthetmathieu@free.fr>
2018-04-09 14:43:43 +0200

    Add ghost evolution in the variante

--- Assets/Scripts/MiniGameFruit/MovingPlatform.cs
+++ Assets/Scripts/MiniGameFruit/MovingPlatform.cs
@@ -17,24 +17,15 @@ public class MovingPlatform : MonoBehaviour {
 	void Update ()
     {
         timerRotation += Time.deltaTime;
-        if(timerRotation > 0.5f)
+        if(timerRotation > 1.0f)
         {
             float action_z = 2f * Mathf.Clamp(vectorActions[0], -1f, 1f);
-           /* if ((gameObject.transform.rotation.z < 0.25f && action_z > 0f) ||
-                (gameObject.transform.rotation.z > -0.25f && action_z < 0f))
-            {*/
-                gameObject.transform.Rotate(new Vector3(0, 0, 1), action_z);
-            //}
+                transform.Rotate(new Vector3(0, 0, 1), action_z);
             float action_x = 2f * Mathf.Clamp(vectorActions[1], -1f, 1f);
-            /*if ((gameObject.transform.rotation.x < 0.25f && action_x > 0f) ||
-                (gameObject.transform.rotation.x > -0.25f && action_x < 0f))
-            {*/
-                gameObject.transform.Rotate(new Vector3(1, 0, 0), action_x);
-            //}
-
-
-            //transform.Rotate(pivot.transform.position, 30.0f);
+                transform.Rotate(new Vector3(1, 0, 0), action_x);
+            
             transform.Rotate(new Vector3(2.0f, 7.0f, 0.0f));
+
             timerRotation = 0.0f;
         }
 	}
