793f060663a924cb9105d11dbf8312b2ecfce4a4
Unknown <transkuja@hotmail.fr>
2018-04-09 18:23:40 +0200

    CFV2: Update LD

---
+++ Assets/AnimEmissive.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimEmissive : MonoBehaviour {
+
+    Material material;
+    public float delay = 5.0f;
+    public float offsetDelay;
+    float time;
+
+    IEnumerator Start () {
+        material = GetComponent<MeshRenderer>().material;
+        material.EnableKeyword("_EMISSION");
+        time = 0;
+        Color baseColor = material.GetColor("_EmissionColor");
+        yield return new WaitForSeconds(offsetDelay);
+
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            Color colorToApply = baseColor;
+
+            while (colorToApply.r < 2.0f)
+            {
+                time += Time.deltaTime*0.25f;
+                colorToApply *= (1 + time);
+                material.SetColor("_EmissionColor", colorToApply);
+                yield return new WaitForEndOfFrame();
+            }
+
+            time = 0.0f;
+            while (colorToApply.r > baseColor.r)
+            {
+                time += Time.deltaTime * 0.25f;
+                colorToApply *= 1 / (1 + time);
+                material.SetColor("_EmissionColor", colorToApply);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+
+}
