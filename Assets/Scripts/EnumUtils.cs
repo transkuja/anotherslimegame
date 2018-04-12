92f406a212d3590be37784fbb6b663b158247ec1
Unknown <transkuja@hotmail.fr>
2018-04-12 15:54:32 +0200

    Je croyais qu'on était amis Seb

--- Assets/Scripts/EnumUtils.cs
+++
@@ -1,2 +0,0 @@
-﻿public enum CollectableType { StrengthEvolution1, PlatformistEvolution1, AgileEvolution1, GhostEvolution1, Points, Rune, Money, Fruits, Bonus, Color, Size };
-public enum Fruit { Clementine, Pomme, Kiwi, Fraise };
--- Assets/Scripts/Utils/EnumUtils.cs
+++ Assets/Scripts/Utils/EnumUtils.cs
@@ -1,7 +1,11 @@
-﻿
+﻿public enum CollectableType { StrengthEvolution1, PlatformistEvolution1, AgileEvolution1, GhostEvolution1, Points, Rune, Money, Fruits, Bonus, Color, Size };
+public enum Fruit { Clementine, Pomme, Kiwi, Fraise };
+
 public enum HitParticles
 {
     HitStar,
     BigHitStar,
     BigHit
 }
+
+public enum RewardType { Rune, Money, Customizable }
