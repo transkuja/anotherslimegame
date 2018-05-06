832097545575c2337c66bf08d76bb1bd5955aebe
Unknown <transkuja@hotmail.fr>
2018-05-06 15:19:16 +0200

    Food: handle 1P2P3P4P ui positions

--- Assets/InputTracksHandler.cs
+++ Assets/InputTracksHandler.cs
@@ -11,7 +11,30 @@ public class InputTracksHandler : MonoBehaviour {
     GameObject inputPrefab;
 
 	public void StartGame() {
-        for (int i = 0; i < GameManager.Instance.CurrentGameMode.curNbPlayers; i++)
+        int nbPlayers = GameManager.Instance.CurrentGameMode.curNbPlayers;
+
+        if (nbPlayers == 1)
+        {
+            transform.GetChild(0).position = (transform.GetChild(1).position + transform.GetChild(2).position) / 2.0f;
+
+        }
+        else if (nbPlayers == 2)
+        {
+            Vector3 tmpPosition = (transform.GetChild(0).position + transform.GetChild(1).position) / 2.0f;
+            transform.GetChild(1).position = (transform.GetChild(2).position + transform.GetChild(3).position) / 2.0f;
+            transform.GetChild(0).position = tmpPosition;
+        }
+        else if (nbPlayers == 3)
+        {
+            transform.GetChild(0).position = (transform.GetChild(0).position + transform.GetChild(1).position) / 2.0f;
+            transform.GetChild(1).position = (transform.GetChild(1).position + transform.GetChild(2).position) / 2.0f;
+            transform.GetChild(2).position = (transform.GetChild(2).position + transform.GetChild(3).position) / 2.0f;
+        }
+
+        for (int i = nbPlayers; i < 4; i++)
+            transform.GetChild(i).gameObject.SetActive(false);
+
+        for (int i = 0; i < nbPlayers; i++)
         {
             GameObject newInput = Instantiate(inputPrefab, tracks[i].transform.GetChild(3)); // child 3 is start position
             newInput.transform.localPosition = Vector3.zero;
