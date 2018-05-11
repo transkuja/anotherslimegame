2dc613140d0e21c8f9909dc76abdf9efacd89b7e
Sebdami <Sebdami@hotmail.fr>
2018-05-11 10:56:23 +0200

    Fix a flamethrower bug

--- Assets/Scripts/FlameThrowerPushDamage.cs
+++ Assets/Scripts/FlameThrowerPushDamage.cs
@@ -15,39 +15,28 @@ public class FlameThrowerPushDamage : MonoBehaviour {
         PlayerCharacterHub playerCharacter = other.GetComponent<PlayerCharacterHub>();
         if (playerCollision != null)
         {
-            if (playerCollision.GetComponent<EvolutionStrength>() != null && playerCharacter.PlayerState == playerCharacter.dashState)
+            if (playerCollision.canBeHit)
             {
-                direction = (transform.position - other.ClosestPointOnBounds(transform.position)).normalized;
-                if (direction == Vector3.zero)
-                    direction = other.transform.forward;
-                Physics.IgnoreCollision(other, this.GetComponent<Collider>(), true);
-            }
-            else
-            {
-                if (playerCollision.canBeHit)
+                playerCollision.canBeHit = false;
+
+                Vector3 centerToTargetCenter = other.transform.position + Vector3.up * 0.5f - transform.position;
+                GameObject go = Instantiate(hitParticles);
+                go.transform.position = transform.position + Vector3.up * 0.5f + centerToTargetCenter / 2.0f;
+                go.transform.rotation = Quaternion.LookRotation(centerToTargetCenter, Vector3.up);
+                Destroy(go, 10.0f);
+                if (AudioManager.Instance != null && AudioManager.Instance.punchFx != null)
+                    AudioManager.Instance.PlayOneShot(AudioManager.Instance.punchFx);
+                UWPAndXInput.GamePad.VibrateForSeconds(playerController.playerIndex, 0.9f, 0.9f, .2f);
+
+                if (GameManager.Instance.CurrentGameMode.TakesDamageFromTraps)
                 {
-                    playerCollision.canBeHit = false;
-
-                    Vector3 centerToTargetCenter = other.transform.position + Vector3.up * 0.5f - transform.position;
-                    GameObject go = Instantiate(hitParticles);
-                    go.transform.position = transform.position + Vector3.up * 0.5f + centerToTargetCenter / 2.0f;
-                    go.transform.rotation = Quaternion.LookRotation(centerToTargetCenter, Vector3.up);
-                    Destroy(go, 10.0f);
-                    if (AudioManager.Instance != null && AudioManager.Instance.punchFx != null)
-                        AudioManager.Instance.PlayOneShot(AudioManager.Instance.punchFx);
-                    UWPAndXInput.GamePad.VibrateForSeconds(playerController.playerIndex, 0.9f, 0.9f, .2f);
-
-                    if (GameManager.Instance.CurrentGameMode.TakesDamageFromTraps)
-                    {
-                        if (GameManager.Instance.IsInHub() && playerCollision.GetComponent<PlayerController>())
-                            playerCollision.DamagePlayerHub();
-                        else
-                            playerCollision.DamagePlayer(other.GetComponent<Player>(), damageOn);
-                    }
-
-                    playerCollision.ExpulsePlayer(other.ClosestPoint(transform.position), other.GetComponent<Rigidbody>(), 50);
+                    if (GameManager.Instance.IsInHub() && playerCollision.GetComponent<PlayerController>())
+                        playerCollision.DamagePlayerHub();
+                    else
+                        playerCollision.DamagePlayer(other.GetComponent<Player>(), damageOn);
                 }
-     
+
+                playerCollision.ExpulsePlayer(other.ClosestPoint(transform.position), other.GetComponent<Rigidbody>(), 50);
             }
         }
 
