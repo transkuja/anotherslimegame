29d6d6f15d47b93ebd57e19d6f981ef420c13f9d
Sebdami <Sebdami@hotmail.fr>
2018-05-11 10:57:12 +0200

    Remove boeuf from Strength Evolution

--- Assets/Scripts/Evolutions/EvolutionStrength.cs
+++ Assets/Scripts/Evolutions/EvolutionStrength.cs
@@ -6,13 +6,10 @@ using UWPAndXInput;
 
 public class EvolutionStrength : EvolutionComponent
 {
-    StatBuff groundSpeedBuff = new StatBuff(Stats.StatType.GROUND_SPEED, 0.85f, -1);
-
     public override void  Start()
     {
         base.Start();
         SetPower(Powers.Strength);
-        playerCharacter.stats.AddBuff(groundSpeedBuff);
         Player playerComponent = GetComponent<Player>();
         if (playerComponent.evolutionTutoShown != null && playerComponent.evolutionTutoShown.Length > 0 && !playerComponent.evolutionTutoShown[(int)Powers.Strength] && !GameManager.Instance.CurrentGameMode.IsMiniGame())
         {
@@ -23,7 +20,6 @@ public class EvolutionStrength : EvolutionComponent
 
     protected override void OnDestroy()
     {
-        playerCharacter.stats.RemoveBuff(groundSpeedBuff);
         base.OnDestroy();
     }
 }
