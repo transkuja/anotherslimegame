2f620a397212f9bb40fad8aeb315e822b784421a
Unknown <transkuja@hotmail.fr>
2018-05-23 16:05:19 +0200

    Change menu background

---
+++ Assets/MoveMenuBackground.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveMenuBackground : MonoBehaviour {
+
+    RawImage rawImage;
+    Rect rect;
+
+    void Start () {
+        rawImage = GetComponent<RawImage>();
+    }
+	
+	void Update () {
+        rect = rawImage.uvRect;
+        rect.x -= Time.deltaTime / 25.0f;
+        rect.y += Time.deltaTime / 25.0f;
+        rawImage.uvRect = rect;
+    }
+}
