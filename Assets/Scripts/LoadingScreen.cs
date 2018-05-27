0d1efcf2dff7cbf796d1160bd7ddf6687c87e50d
Unknown <Sebdami@hotmail.fr>
2018-05-27 20:37:37 +0200

    Remove useless and deprecated function

--- Assets/Scripts/LoadingScreen.cs
+++ Assets/Scripts/LoadingScreen.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class LoadingScreen : MonoBehaviour {
 
-    private void OnLevelWasLoaded(int level)
-    {
-        GameManager.Instance.FadeManager.FadeIn(0.2f);
-    }
-
     private void Start()
     {
         StartCoroutine(LevelLoader.LoadLevelAsync(LevelLoader.TargetLevelId, true));
