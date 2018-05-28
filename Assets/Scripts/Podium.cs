e4e1541775b04ef14f70e10eed4d4e7ba8e51222
Unknown <transkuja@hotmail.fr>
2018-05-28 14:31:16 +0200

    Fix podium

--- Assets/Scripts/Podium.cs
+++ Assets/Scripts/Podium.cs
@@ -16,36 +16,38 @@ public class Podium : MonoBehaviour {
             transform.GetChild(i).gameObject.SetActive(true);
             PlayerCosmetics playerCosmetics = transform.GetChild(i).GetComponentInChildren<PlayerCosmetics>();
 
-            if (GameManager.Instance.DataContainer.colorFadeSelected[i])
+            int indexRanked = container.lastRanks[i];
+
+            if (GameManager.Instance.DataContainer.colorFadeSelected[indexRanked])
                 playerCosmetics.ColorFadeType = ColorFadeType.Basic;
             else
-                playerCosmetics.SetUniqueColor(GameManager.Instance.DataContainer.selectedColors[i]);
-            playerCosmetics.FaceType = GameManager.Instance.DataContainer.selectedFaces[i];
+                playerCosmetics.SetUniqueColor(GameManager.Instance.DataContainer.selectedColors[indexRanked]);
+            playerCosmetics.FaceType = GameManager.Instance.DataContainer.selectedFaces[indexRanked];
 
             if (playerCosmetics.transform.GetComponentInChildren<CustomizableSockets>() != null)
             {
                 Transform customizableParent = playerCosmetics.transform.GetComponentInChildren<CustomizableSockets>().transform;
 
                 // Init mustaches //
-                playerCosmetics.Mustache = GameManager.Instance.DataContainer.mustachesSelected[i];
+                playerCosmetics.Mustache = GameManager.Instance.DataContainer.mustachesSelected[indexRanked];
 
                 // Init ears //
-                playerCosmetics.Ears = GameManager.Instance.DataContainer.earsSelected[i];
+                playerCosmetics.Ears = GameManager.Instance.DataContainer.earsSelected[indexRanked];
 
                 // Init hats //
-                playerCosmetics.Hat = GameManager.Instance.DataContainer.hatsSelected[i];
+                playerCosmetics.Hat = GameManager.Instance.DataContainer.hatsSelected[indexRanked];
 
                 // Init forehead //
-                playerCosmetics.Forehead = GameManager.Instance.DataContainer.foreheadsSelected[i];
+                playerCosmetics.Forehead = GameManager.Instance.DataContainer.foreheadsSelected[indexRanked];
 
                 // Init skin //
-                playerCosmetics.Skin = GameManager.Instance.DataContainer.skinsSelected[i];
+                playerCosmetics.Skin = GameManager.Instance.DataContainer.skinsSelected[indexRanked];
 
                 // Init chin //
-                playerCosmetics.Chin = GameManager.Instance.DataContainer.chinsSelected[i];
+                playerCosmetics.Chin = GameManager.Instance.DataContainer.chinsSelected[indexRanked];
 
                 // Init accessory //
-                playerCosmetics.Accessory = GameManager.Instance.DataContainer.accessoriesSelected[i];
+                playerCosmetics.Accessory = GameManager.Instance.DataContainer.accessoriesSelected[indexRanked];
             }
         }
     }
